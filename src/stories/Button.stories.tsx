2fdc4111d43683501c6d5a5e0a120115dd95afce
Chris Evans <evanschris@users.noreply.github.com>
2020-11-27 14:03:10 +0000

    convert to typescript

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { Button } from './Button';
+import { Button, ButtonProps } from './Button';
 
 export default {
-  title: 'Components/Button',
-  component: Button
-};
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
 
-const Template = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
