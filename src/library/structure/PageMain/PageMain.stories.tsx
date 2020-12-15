91c544b7666492f02737e47294cd2872c3b2e50f
Chris Evans <evanschris@users.noreply.github.com>
2020-12-15 14:27:42 +0000

    add in basic table styles

--- src/library/structure/PageMain/PageMain.stories.tsx
+++ src/library/structure/PageMain/PageMain.stories.tsx
@@ -15,3 +15,156 @@ export const PageMainExample = Template.bind({});
 PageMainExample.args = {
   classes: 'testclass',
 };
+
+
+export const ExampleOfTables = () =>
+  <PageMain>
+    <p>This is an example container for page main including a table:</p>
+    <table>
+      <tbody>
+          <tr>
+              <td>Monday</td>
+              <td>Closed</td>
+          </tr>
+          <tr>
+              <td>Tuesday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Wednesday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Thursday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Friday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Saturday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Sunday</td>
+              <td>Closed</td>
+          </tr>
+      </tbody>
+    </table>
+
+    <p>A table with header rows</p>
+    <table>
+      <thead>
+          <tr>
+              <th>Date</th>
+              <th>Status</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>Monday</td>
+              <td>Closed</td>
+          </tr>
+          <tr>
+              <td>Tuesday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Wednesday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Thursday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Friday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Saturday</td>
+              <td>10:30 — 19:00</td>
+          </tr>
+          <tr>
+              <td>Sunday</td>
+              <td>Closed</td>
+          </tr>
+      </tbody>
+  </table>
+
+  <p>A table with both header rows</p>
+  <table>
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>Monday</th>
+            <td>Closed</td>
+        </tr>
+        <tr>
+            <th>Tuesday</th>
+            <td>10:30 — 19:00</td>
+        </tr>
+        <tr>
+            <th>Wednesday</th>
+            <td>10:30 — 19:00</td>
+        </tr>
+        <tr>
+            <th>Thursday</th>
+            <td>10:30 — 19:00</td>
+        </tr>
+        <tr>
+            <th>Friday</th>
+            <td>10:30 — 19:00</td>
+        </tr>
+        <tr>
+            <th>Saturday</th>
+            <td>10:30 — 19:00</td>
+        </tr>
+        <tr>
+            <th>Sunday</th>
+            <td>Closed</td>
+        </tr>
+    </tbody>
+  </table>
+
+  <p>A table with side heading row with caption</p>
+  <table>
+    <caption>test caption</caption>
+    <tbody>
+      <tr>
+          <th>Monday</th>
+          <td>Closed</td>
+      </tr>
+      <tr>
+          <th>Tuesday</th>
+          <td>10:30 — 19:00</td>
+      </tr>
+      <tr>
+          <th>Wednesday</th>
+          <td>10:30 — 19:00</td>
+      </tr>
+      <tr>
+          <th>Thursday</th>
+          <td>10:30 — 19:00</td>
+      </tr>
+      <tr>
+          <th>Friday</th>
+          <td>10:30 — 19:00</td>
+      </tr>
+      <tr>
+          <th>Saturday</th>
+          <td>10:30 — 19:00</td>
+      </tr>
+      <tr>
+          <th>Sunday</th>
+          <td>Closed</td>
+      </tr>
+    </tbody>
+  </table>
+</PageMain>
--- src/library/structure/PageMain/PageMain.styles.js
+++ src/library/structure/PageMain/PageMain.styles.js
@@ -1,4 +1,63 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
+
+
+
+    // TABLE STYLES
+    table { 
+        width: 750px; 
+        border-collapse: collapse; 
+        margin: 50px auto;
+        max-width: 100%;
+        overflow: scroll;
+
+        caption {
+            width: 100%;
+            display: block;
+            text-align: left;
+            padding-left: 10px;
+            font-size: 1.2em;
+            margin-bottom: 15px;
+        }
+    }
+
+    tr:nth-of-type(even) { 
+        background: ${props => props.theme.theme_vars.colours.grey_light}; 
+    }
+
+    td, th { 
+        padding: 10px; 
+        border: 1px solid ${props => props.theme.theme_vars.colours.grey}; 
+        text-align: left; 
+        font-size: 18px;
+    }
+    th {
+        background: ${props => props.theme.theme_vars.colours.action_dark}; 
+        color: ${props => props.theme.theme_vars.colours.white}; 
+    }
+
+
+    @media only screen and (max-width: ${props => props.theme.theme_vars.breakpoints.m}){
+        table { 
+            width: 100%; 
+        }
+
+        table, thead, tbody, th, td, tr { 
+            display: block; 
+        }
+        
+        thead tr { 
+            position: absolute;
+            top: -9998px;
+            left: -9998px;
+        }
+        
+        tr { border: 1px solid ${props => props.theme.theme_vars.colours.grey}; }
+        
+        td, th { 
+            border: none;
+            position: relative;
+        }
+    }
 `
