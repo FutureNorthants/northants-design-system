9f67de4c6d1571dc37079e64d8f9b89051e25855
Chris Evans <evanschris@users.noreply.github.com>
2021-01-05 14:14:23 +0000

    update table styles

--- src/library/structure/PageMain/PageMain.styles.js
+++ src/library/structure/PageMain/PageMain.styles.js
@@ -56,59 +56,56 @@ export const Container = styled.main`
     }
 
     // TABLE STYLES
+    .table_container {
+        @media only screen and (max-width: ${props => props.theme.theme_vars.breakpoints.s}){
+            max-width: 100%;
+            overflow: scroll;
+        }
+    }
     table { 
-        width: 750px; 
+        width: 100%; 
         border-collapse: collapse; 
         margin: 50px auto;
         max-width: 100%;
         overflow: scroll;
 
         caption {
-            width: 100%;
-            display: block;
             text-align: left;
             padding-left: 10px;
-            font-size: 1.2em;
             margin-bottom: 15px;
+            font-weight: bold;
+        }
+    }
+
+    tr {
+        border-bottom: 1px solid ${props => props.theme.theme_vars.colours.grey};
+        &:hover {
+            background: ${props => props.theme.theme_vars.colours.grey_light} !important; 
         }
     }
 
     tr:nth-of-type(even) { 
-        background: ${props => props.theme.theme_vars.colours.grey_light}; 
+        background: ${props => props.theme.theme_vars.colours.grey_light}40; 
     }
 
     td, th { 
         padding: 10px; 
-        border: 1px solid ${props => props.theme.theme_vars.colours.grey}; 
         text-align: left; 
-        font-size: 18px;
     }
     th {
-        background: ${props => props.theme.theme_vars.colours.action_dark}; 
-        color: ${props => props.theme.theme_vars.colours.white}; 
+        font-weight: bold;
+    }
+    thead tr {
+        border-bottom: 1px solid ${props => props.theme.theme_vars.colours.black};
+        &:hover {
+            background: transparent !important; 
+        }
     }
 
 
-    @media only screen and (max-width: ${props => props.theme.theme_vars.breakpoints.m}){
+    @media only screen and (max-width: ${props => props.theme.theme_vars.breakpoints.s}){
         table { 
             width: 100%; 
-        }
-
-        table, thead, tbody, th, td, tr { 
-            display: block; 
-        }
-        
-        thead tr { 
-            position: absolute;
-            top: -9998px;
-            left: -9998px;
-        }
-        
-        tr { border: 1px solid ${props => props.theme.theme_vars.colours.grey}; }
-        
-        td, th { 
-            border: none;
-            position: relative;
         }
     }
 `
