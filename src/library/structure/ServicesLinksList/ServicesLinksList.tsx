e270ee3433488be7736dacce99ed34b92614a6b8
Chris Evans <evanschris@users.noreply.github.com>
2021-01-07 14:18:39 +0000

    hide some services on mobile

--- src/library/structure/ServicesLinksList/ServicesLinksList.tsx
+++ src/library/structure/ServicesLinksList/ServicesLinksList.tsx
@@ -1,45 +1,54 @@
-
-import React from "react";
+import React, { useState } from "react";
 
 import { ServicesLinksListProps } from "./ServicesLinksList.types";
 import * as Styles from "./ServicesLinksList.styles";
 
 const ServicesLinksList: React.FC<ServicesLinksListProps> = ({ 
     serviceLinksArray 
-}) => (
-    <Styles.Container id="all-services">
-        <Styles.LinksList>
-            {serviceLinksArray.map((link) =>
-                <Styles.PagelinkBlock key={link.url}>
-                    <Styles.ServiceTitleLink 
-                        href={link.url} 
-                        title={"Go to " + link.title}
-                    >
-                        <Styles.ServiceTitle text={link.title} />
-                    </Styles.ServiceTitleLink>
+}) => { 
+    const [open, setOpen] = useState(false);
+    return(
+        <>
+            <Styles.Container id="all-services" className={open && "open"}>
+                <Styles.LinksList>
+                    {serviceLinksArray.map((link) =>
+                        <Styles.PagelinkBlock key={link.url}>
+                            <Styles.ServiceTitleLink 
+                                href={link.url} 
+                                title={"Go to " + link.title}
+                            >
+                                <Styles.ServiceTitle text={link.title} />
+                            </Styles.ServiceTitleLink>
 
-                    {link.quickLinksArray.length > 0 && 
-                        <Styles.QuicklinkList>
-                            {link.quickLinksArray.map((quicklink) =>
-                                <Styles.QuicklinkItem key={quicklink.url}>
-                                    <Styles.Quicklink 
-                                        href={quicklink.url} 
-                                        title={"Go to " + quicklink.title}
-                                    >
-                                        {quicklink.title}
-                                    </Styles.Quicklink>
-                                </Styles.QuicklinkItem>
-                            )}
-                        </Styles.QuicklinkList>
+                            {link.quickLinksArray.length > 0 && 
+                                <Styles.QuicklinkList>
+                                    {link.quickLinksArray.map((quicklink) =>
+                                        <Styles.QuicklinkItem key={quicklink.url}>
+                                            <Styles.Quicklink 
+                                                href={quicklink.url} 
+                                                title={"Go to " + quicklink.title}
+                                            >
+                                                {quicklink.title}
+                                            </Styles.Quicklink>
+                                        </Styles.QuicklinkItem>
+                                    )}
+                                </Styles.QuicklinkList>
+                            }
+                        </Styles.PagelinkBlock>
+                    )}
+                    {serviceLinksArray.length > 1 && ((serviceLinksArray.length + 1) % 3 === 0) &&
+                        <Styles.PagelinkBlank />
                     }
-                </Styles.PagelinkBlock>
-            )}
-            {serviceLinksArray.length > 1 && ((serviceLinksArray.length + 1) % 3 === 0) &&
-                <Styles.PagelinkBlank />
-            }
-        </Styles.LinksList>
-    </Styles.Container>
-);
+                </Styles.LinksList>
+            </Styles.Container>
+            <Styles.ViewMoreButtonContainer>
+                <Styles.ViewMoreButton onClick={() => open ? setOpen(false) : setOpen(true)}>
+                    View { open ? "less" : "more" } services
+                </Styles.ViewMoreButton>
+            </Styles.ViewMoreButtonContainer>
+        </>
+    );
+}
 
 export default ServicesLinksList;
 
