ee9997bbdda09bc44cbe23a433ff2adcd79bf9a5
Chris Evans <evanschris@users.noreply.github.com>
2021-01-08 11:29:48 +0000

    convert divider into hr

--- src/library/slices/Divider/Divider.styles.js
+++ src/library/slices/Divider/Divider.styles.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
-export const Divider = styled.div`
+export const Divider = styled.hr`
   display: block;
   width: 100%;
   height: 2px;
   margin: ${props => props.theme.theme_vars.spacingSizes.medium} 0;
   background-color: ${props => props.theme.theme_vars.colours.black}80;
+  border: none;
 `
 
