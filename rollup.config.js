0eee0381c49cd37cd4c96979b52b41464e589f1f
Chris Evans <evanschris@users.noreply.github.com>
2021-01-29 11:44:46 +0000

    allow json files in rollup for axios package

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";import svg from 'rollup-plugin-svg'
+import json from 'rollup-plugin-json';
 
 const packageJson = require("./package.json");
 
@@ -33,6 +34,26 @@ export default {
     }),
     commonjs(),
     svg(),
-    typescript({ useTsconfigDeclarationDir: true })
+    typescript({ useTsconfigDeclarationDir: true }),
+    json({
+      // All JSON files will be parsed by default,
+      // but you can also specifically include/exclude files
+      include: 'node_modules/**',
+      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
+
+      // for tree-shaking, properties will be declared as
+      // variables, using either `var` or `const`
+      preferConst: true, // Default: false
+
+      // specify indentation for the generated default export —
+      // defaults to '\t'
+      indent: '  ',
+
+      // ignores indent and generates the smallest code
+      compact: true, // Default: false
+
+      // generate a named export for every property of the JSON object
+      namedExports: true // Default: true
+    })
   ]
 };
