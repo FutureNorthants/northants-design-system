00cb480c0f70471753b33db478df6c37ae834484
Chris Evans <evanschris@users.noreply.github.com>
2021-02-10 16:39:36 +0000

    fix homepage example

--- src/library/pages/Home/HomePage.tsx
+++ src/library/pages/Home/HomePage.tsx
@@ -1,54 +1,59 @@
 import React from 'react';
 import * as PageStructures from '../../structure/PageStructures';
 import Heading from '../../components/Heading/Heading';
-import Searchbar from '../../structure/Searchbar/Searchbar';
 
 export interface HomePageProps {
 
 }
 
+const submitInfo = [{
+  postTo: "/search",
+  params: {
+      type: "search"
+  }
+}]
+
+
 export const HomePage: React.FC<HomePageProps> = ({  }) => (
   <>
-  <PageStructures.CookieBanner 
-  title="We use cookies on this site to enhance your user experience"
-  paragraph={<p>By clicking the Accept button, you agree to us doing so. <a href="#">More info on our cookie policy</a></p>}
-  acceptButtonText="Accept cookies policy"
-  rejectButtonText= "No, thanks"
-  acceptCallback={() => {
-    console.log('loading trackers');
-    var tag = document.createElement("script");
-    tag.src = "https://www.googletagmanager.com/gtag/js?id=<%= ENV[\"GTM_TRACKING_ID\"] %>";
-    document.getElementsByTagName("head")[0].appendChild(tag);
+    <PageStructures.CookieBanner 
+      title="We use cookies on this site to enhance your user experience"
+      paragraph={<p>By clicking the Accept button, you agree to us doing so. <a href="#">More info on our cookie policy</a></p>}
+      acceptButtonText="Accept cookies policy"
+      rejectButtonText= "No, thanks"
+      acceptCallback={() => {
+        console.log('loading trackers');
+        var tag = document.createElement("script");
+        tag.src = "https://www.googletagmanager.com/gtag/js?id=<%= ENV[\"GTM_TRACKING_ID\"] %>";
+        document.getElementsByTagName("head")[0].appendChild(tag);
 
 
 
-    window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', '<%= ENV["GA_TRACKING_ID"] %>');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', '<%= ENV["GA_TRACKING_ID"] %>');
 
 
-    (function (h, o, t, j, a, r) {
-        h.hj = h.hj || function () {
-            (h.hj.q = h.hj.q || []).push(arguments)
-        };
-        h._hjSettings = {
-            hjid: 12345,
-            hjsv: 6
-        };
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script');
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+        (function (h, o, t, j, a, r) {
+            h.hj = h.hj || function () {
+                (h.hj.q = h.hj.q || []).push(arguments)
+            };
+            h._hjSettings = {
+                hjid: 12345,
+                hjsv: 6
+            };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 
-}}
-  
-  
+    }}
   />
   <PageStructures.Header isHomepage />
   <PageStructures.MaxWidthContainer>
@@ -57,7 +62,7 @@ export const HomePage: React.FC<HomePageProps> = ({  }) => (
 
         <p>This is where the example home page would go - combining components to create an example of a total page together</p>
 
-        <Searchbar isLarge isLight />
+        <PageStructures.Searchbar isLarge isLight submitInfo={submitInfo} />
 
         <PageStructures.ServicesLinksList 
           serviceLinksArray={[
