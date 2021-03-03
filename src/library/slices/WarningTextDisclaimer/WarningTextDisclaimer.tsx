eeccf10e53b58ca563350a51cf80ec49c6b70b68
Chris Evans <evanschris@users.noreply.github.com>
2021-03-03 10:58:55 +0000

    set disclaimer to yellow

--- src/library/slices/WarningTextDisclaimer/WarningTextDisclaimer.tsx
+++ src/library/slices/WarningTextDisclaimer/WarningTextDisclaimer.tsx
@@ -8,7 +8,7 @@ import WarningText from "../WarningText/WarningText";
 // @TODO this needs to fit within the content border 
 
 const WarningTextDisclaimer: React.FC<WarningTextDisclaimerProps> = () => (
-    <WarningText title="Why information is on a different website">
+    <WarningText title="Why information is on a different website" isWarning>
         We are in the process of adding information to this new unitary council website. Some pages will give you a link back to a previous council website to help you find what you need. <a href="/">Read more about the changes</a>.
     </WarningText>
 );
