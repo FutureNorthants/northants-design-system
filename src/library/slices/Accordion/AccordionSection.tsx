8ee9b8527b02aeed44c689610cd06bc3fe66ef75
Chris Evans <evanschris@users.noreply.github.com>
2021-03-25 14:51:44 +0000

    add title to accordion button

--- src/library/slices/Accordion/AccordionSection.tsx
+++ src/library/slices/Accordion/AccordionSection.tsx
@@ -11,7 +11,7 @@ const AccordionSection: React.FC<AccordionSectionProps> = ({ title, content, sum
                 <Styles.SectionHeader onClick={onSectionToggle}>
                     
                     <Styles.SectionHeading as={isFilter ? "h3" : "h2"}>
-                        <Styles.SectionButton isFilter={isFilter} type="button" id={`${accordionSectionId}-heading`} aria-controls={`${accordionSectionId}-content`} aria-expanded={isExpanded ? "true" : "false"}>
+                        <Styles.SectionButton title={isExpanded ? "Collapse section" : "Expand section"} isFilter={isFilter} type="button" id={`${accordionSectionId}-heading`} aria-controls={`${accordionSectionId}-content`} aria-expanded={isExpanded ? "true" : "false"}>
                           {title}
                           <Styles.AccordionIcon aria-hidden="true"></Styles.AccordionIcon>
                         </Styles.SectionButton>
