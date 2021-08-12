297431499495a546a8bf9814fa934287c356d201
Anthony Taylor <antaylor@northamptonshire.gov.uk>
2021-08-12 10:55:49 +0100

    removing go to from promoted links

--- src/library/components/PromotedLinks/PromotedLinks.tsx
+++ src/library/components/PromotedLinks/PromotedLinks.tsx
@@ -9,7 +9,7 @@ const PromotedLinks: React.FC<PromotedLinksProps> = ({ promotedLinksArray, oneCo
         return (
             <Styles.PromotedLinks data-testid="PromotedLinks" oneCol={oneCol} >
             {promotedLinksArray.map((link, i) =>
-                <Styles.PromotedLink key={i} oneCol={oneCol} href={link.url} title={"Go to " + link.title}>
+                <Styles.PromotedLink key={i} oneCol={oneCol} href={link.url} title={link.title}>
                     <span>{link.title}</span>
                 </Styles.PromotedLink>
                 )}
