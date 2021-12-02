f275471ed76bf9391d536b5ef51082d8c3b15a8e
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2021-12-02 13:14:07 +0000

    Fixed visual issue on story example

--- src/library/pages/NewsArticle/NewsArticle.tsx
+++ src/library/pages/NewsArticle/NewsArticle.tsx
@@ -1,8 +1,6 @@
-
 import React from "react";
 import * as PageStructures from '../../structure/PageStructures';
 import Heading from '../../components/Heading/Heading';
-
 import BlockQuote from '../../slices/BlockQuote/BlockQuote';
 import Button from "../../components/Button/Button";
 import NewsArticleDate from "../../structure/NewsArticleDate/NewsArticleDate";
@@ -16,9 +14,8 @@ export interface NewsArticleProps {
   isOld?: boolean;
 }
 
-
 export const NewsArticle: React.FC<NewsArticleProps> = ({ withImage, imagePlaceholder, isOld = false }) => (
-    <>
+  <>
   <PageStructures.Header />
   <PageStructures.MaxWidthContainer>
       <PageStructures.PageMain>
@@ -40,19 +37,18 @@ export const NewsArticle: React.FC<NewsArticleProps> = ({ withImage, imagePlaceh
         <p>In Northamptonshire £750,000 from the grant is being used to provide shopping vouchers for families with children who are eligible for Free School Meals with vouchers being distributed to the families by their schools.</p>
 
         <p>Cllr Fiona Baker, county council cabinet member for Children, Families and Education, said:</p>
-        <BlockQuote quote="<p>“I’m delighted that we’re able to continue this support for the families and pupils who are in most need of our help.</p>
+
+        <BlockQuote quote={<><p>“I’m delighted that we’re able to continue this support for the families and pupils who are in most need of our help.</p>
 
         <p>“Many have talked about being right in the eye of the storm with regard to the Covid pandemic so it’s important that we can offer this lifeline to those families and children who may be facing hardship.</p>
 
-        <p>“I’m very pleased that we have been allocated this money from the government as it will help make sure that we can continue to do everything we can to support people in the county through this pandemic.“</p>" />
+        <p>“I’m very pleased that we have been allocated this money from the government as it will help make sure that we can continue to do everything we can to support people in the county through this pandemic.“</p></>} />
 
         <p>The county council has been given £2,080,000 from the grant which aims to provide support for those households who are most in need with the cost of food, energy and water bills as well as other associated costs. It is primarily intended to support families with children, with 80% of the funding earmarked for these groups.</p>
 
         <p>The rest of the grant funding has been allocated to the Household Winter Support Scheme. This scheme builds on existing community resilience work to use a network of established partner organisations to distribute a one-time voucher of support to help those most in need. Individuals will not be able to for a voucher directly, instead partner organisations will be able to apply for vouchers on behalf of eligible households.</p>
 
         <p>The county council has also been allocated £464,280 to support the Clinically Extremely Vulnerable with any hardship they may be facing as a result of the need to take additional precautions, particularly during periods where the virus is more prevalent.</p>
-
-
 
         <DisplayDate preText="Last updated " text="1614178638" format="X" />
 
