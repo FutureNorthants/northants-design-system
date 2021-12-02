498384b6ffde6905143bbed3cb53d9c34ee304f4
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2021-12-02 13:14:53 +0000

    Fixed SSR window error

--- src/library/structure/AlertBanner/AlertBanner.tsx
+++ src/library/structure/AlertBanner/AlertBanner.tsx
@@ -40,6 +40,7 @@ function useLocalStorage(key, initialValue) {
     // State to store our value
     // Pass initial state function to useState so logic is only executed once
     const [storedValue, setStoredValue] = useState(() => {
+      if (typeof window === 'undefined') return initialValue; // e.g. at server-side build time
       try {
         // Get from local storage by key
         const item = window.localStorage.getItem(key);
@@ -55,6 +56,7 @@ function useLocalStorage(key, initialValue) {
     // Return a wrapped version of useState's setter function that ...
     // ... persists the new value to localStorage.
     const setValue = value => {
+      if (typeof window === 'undefined') return; // e.g. at server-side build time
       try {
         // Allow value to be a function so we have same API as useState
         const valueToStore =
