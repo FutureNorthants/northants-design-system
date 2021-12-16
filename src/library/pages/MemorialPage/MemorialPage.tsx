6dd4d7a4fae5424f399cc7ec3b7969831cbc54c4
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2021-12-16 16:28:00 +0000

    Fix promo spacing, update home page examples to match frontend and DRY up

--- src/library/pages/MemorialPage/MemorialPage.tsx
+++ src/library/pages/MemorialPage/MemorialPage.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import * as PageStructures from '../../structure/PageStructures';
 import AlertBanner from '../../structure/AlertBanner/AlertBanner';
-import HomeUnitarySection from '../../structure/HomeUnitarySection/HomeUnitarySection';
 import { newsArticleData } from '../../structure/NewsArticleFeaturedBlock/NewsArticleFeaturedBlockData';
-
 import ServicesLinksList from "../../structure/ServicesLinksList/ServicesLinksList";
-import PromotedLinks from "../../components/PromotedLinks/PromotedLinks";
 import { ThemeProvider } from 'styled-components';
 import { GDS_theme, north_theme, west_theme, lb_theme_north, lb_theme_west } from '../../../themes/theme_generator';
-export interface MemorialPageProps {
-  heroArray: Array<HeroImageProp>;
-  servicesArray: Array<PageLinkProp>;
-}
-interface HeroImageProp {
-  /**
-   * The url of the image
-   */
-  image1440x810: string;
-  /**
-   * The url of the image in 10x smaller for lazy loading
-   */
-  image144x81: string;
-  /**
-   * Optional alt text for the image - this should only be included if the image contains content that is important to users and not purely decorative
-   */
-  imageAltText?: string;
-}
+import { promoData } from '../../structure/PromoBlock/PromoBlock.storydata';
+import { PageLinkProp } from '../../structure/ServicesLinksList/ServicesLinksList.types';
 
+export interface MemorialPageProps {
+  /**
+   * The number of promotional tiles (0 to 4 supported)
+   */
+   numberOfPromos: number;
+  
+   /**
+    * The number of news stories (0 to 9 supported)
+    */
+   numberOfNewsStories: number;
+
+   /**
+    * Service links
+    */
+   servicesArray: Array<PageLinkProp>;
+}
 
 const submitInfo = [{
   postTo: "/search",
@@ -35,7 +32,6 @@ const submitInfo = [{
   }
 }]
 const AlertMessage = <p>Coronavirus | National lockdown: stay at home. <a href="/">Learn what this means for residents and workers here</a></p>
-
 
 const serviceLinksArray = [
   {
@@ -50,7 +46,6 @@ const serviceLinksArray = [
     ]
   },
 ];
-
 
 const councilServicesLinksArray = [
   {
@@ -93,7 +88,6 @@ const memorialServiceLinksArray = [
   }
 ];
 
-
 const ExampleMemorialHeroArgs = {
   src: "/hero-image.png",
   placeholder: "/hero-image-small.png",
@@ -103,10 +97,12 @@ const ExampleMemorialHeroArgs = {
   theme:  north_theme,
 };
 
-export const MemorialPage: React.FC<MemorialPageProps> = ({ heroArray, servicesArray }) => {
-  
-  
-  // console.log(decorators);
+/**
+ * An example memorialised home page layout constructed from the structures and components defined
+ * in the design system. The actual page layout is defined within the front end app and may be
+ * out of step with this example.
+ */
+ export const MemorialPage: React.FunctionComponent<MemorialPageProps> = ({ numberOfPromos = 4, numberOfNewsStories = 3, servicesArray }) => {
   return (
   <>
     <PageStructures.CookieBanner 
@@ -119,9 +115,6 @@ export const MemorialPage: React.FC<MemorialPageProps> = ({ heroArray, servicesA
         var tag = document.createElement("script");
         tag.src = "https://www.googletagmanager.com/gtag/js?id=<%= ENV[\"GTM_TRACKING_ID\"] %>";
         document.getElementsByTagName("head")[0].appendChild(tag);
-
-
-
         window.dataLayer = window.dataLayer || [];
 
         function gtag() {
@@ -129,7 +122,6 @@ export const MemorialPage: React.FC<MemorialPageProps> = ({ heroArray, servicesA
         }
         gtag('js', new Date());
         gtag('config', '<%= ENV["GA_TRACKING_ID"] %>');
-
 
         (function (h, o, t, j, a, r) {
             h.hj = h.hj || function () {
@@ -150,101 +142,71 @@ export const MemorialPage: React.FC<MemorialPageProps> = ({ heroArray, servicesA
   />
   <AlertBanner title="Coronavirus (COVID-19)" uid="homealert" children={AlertMessage} />
 
-
   <ThemeProvider theme={lb_theme_north}>
     <PageStructures.Header />
+
     <PageStructures.MemorialHero {...ExampleMemorialHeroArgs} />
-
-
     
     <PageStructures.PageWrapper colour="grey_light">
-    <PageStructures.MaxWidthContainer noBackground={true}>
-      <ServicesLinksList  hideHeader={true} serviceLinksArray={memorialServiceLinksArray}  /> 
-      <PageStructures.NewsArticleFeaturedBlock articles={newsArticleData.articles} viewAllLink="/news/" />
-    </PageStructures.MaxWidthContainer>
+      <PageStructures.MaxWidthContainer noBackground={true}>
+        <ServicesLinksList  hideHeader={true} serviceLinksArray={memorialServiceLinksArray}  /> 
+        <PageStructures.NewsArticleFeaturedBlock articles={newsArticleData.articles.slice(0, numberOfNewsStories)} viewAllLink="/news/" />
+      </PageStructures.MaxWidthContainer>
     </PageStructures.PageWrapper>
-  </ThemeProvider>
 
+    <PageStructures.MaxWidthContainer>
+        <PageStructures.PageMain>
+          <PageStructures.ServicesLinksList 
+            serviceLinksArray={servicesArray}
+          />
 
-  <PageStructures.MaxWidthContainer>
-      <PageStructures.PageMain>
+          { numberOfPromos > 0 &&
+            <>
+              <PageStructures.PromoBanner
+                title="Volunteer at a local Covid-19 vaccine centre"
+                ctaUrl="/"
+                ctaText="How to become a volunteer"
+                image1440x810="https://images.unsplash.com/photo-1612277795315-26dd8dcdc8a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+                image144x81="https://imgur.com/lIHCSwV.jpg"
+              >
+                <p>We’re looking for people to come forward and help deliver the Covid-19 vaccination programme in North Northamptonshire.</p>
+              </PageStructures.PromoBanner>
+              <PageStructures.PromoBlock promos={promoData.promos.slice(0, numberOfPromos - 1)} />
+            </>
+          }
 
-        <PageStructures.ServicesLinksList 
-          serviceLinksArray={servicesArray}
+        </PageStructures.PageMain>
+      </PageStructures.MaxWidthContainer>
+      
+      <PageStructures.Footer
+        footerLinksArray={[
+          {
+            title: "About",
+            url: "/"
+          },
+          {
+            title: "Accessibility",
+            url: "/"
+          },
+          {
+            title: "Cookies",
+            url: "/"
+          },
+          {
+            title: "Contact us",
+            url: "/"
+          },
+          {
+            title: "Jobs",
+            url: "/"
+          },
+          {
+            title: "Newsletter",
+            url: "/"
+          }
+        ]}
         />
-        <PageStructures.PromoBanner 
-          title="Volunteer at a local Covid-19 vaccine centre"
-          ctaUrl="/"
-          ctaText="How to become a volunteer"
-          image1440x810="https://images.unsplash.com/photo-1612277795315-26dd8dcdc8a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          image144x81="https://imgur.com/lIHCSwV.jpg"
-        >
-          <p>We’re looking for people to come forward and help deliver the Covid-19 vaccination programme in North Northamptonshire.</p>
-        </PageStructures.PromoBanner>
-
-        <HomeUnitarySection />
-
-        <PageStructures.NewsArticleFeaturedBlock articles={newsArticleData.articles} viewAllLink="/news/" />
-
-      </PageStructures.PageMain>
-    </PageStructures.MaxWidthContainer>
-    <PageStructures.Footer 
-      footerLinksArray={[
-        {
-          title: "About",
-          url: "/"
-        },
-        {
-          title: "Accessibility",
-          url: "/"
-        },
-        {
-          title: "Cookies",
-          url: "/"
-        },
-        {
-          title: "Contact us",
-          url: "/"
-        },
-        {
-          title: "Jobs",
-          url: "/"
-        },
-        {
-          title: "Newsletter",
-          url: "/"
-        }
-      ]}
-    />
+    </ThemeProvider>
   </>
 )
 };
-
-interface PageLinkProp {
-  /**
-  * Title of the page
-  */
-  title: string;
-  /**
-  * URL of the page
-  */
-  url: string;
-  /**
-  * URL of the svg icon for the service landing page
-  */
-  iconKey?: string;
-  /**
-  * Array of quick links for the service
-  */
- quickLinksArray: Array<QuickLinkProp>;
-}
-interface QuickLinkProp {
-/**
-* Title of the page
-*/
-title: string;
-/**
-* URL of the page
-*/
-url: string;
-}
