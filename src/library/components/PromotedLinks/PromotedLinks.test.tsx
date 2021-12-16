e8e1c0457d82424cce886e391e793a1e0e405de2
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2021-12-16 11:23:20 +0000

    Fix tests

--- src/library/components/PromotedLinks/PromotedLinks.test.tsx
+++ src/library/components/PromotedLinks/PromotedLinks.test.tsx
@@ -1,27 +1,42 @@
-
 import React from "react";
 import { render } from "@testing-library/react";
 
 import PromotedLinks from "./PromotedLinks";
 import { PromotedLinksProps } from "./PromotedLinks.types";
+import { ThemeProvider } from "styled-components";
+import { west_theme } from "../../../themes/theme_generator";
 
 describe("Test Component", () => {
   let props: PromotedLinksProps;
 
   beforeEach(() => {
     props = {
-      foo: "bar"
+      promotedLinksArray: [
+        {
+          title: "The first link",
+          url: "/the-first-url/",
+        },
+        {
+          title: "The second link",
+          url: "/the-second-url/",
+        },
+      ],
+      oneCol: true,
     };
   });
 
-  const renderComponent = () => render(<PromotedLinks {...props} />);
+  const renderComponent = () =>
+    render(
+      <ThemeProvider theme={west_theme}>
+        <PromotedLinks {...props} />
+      </ThemeProvider>
+    );
 
   it("should render foo text correctly", () => {
-    props.foo = "example content";
     const { getByTestId } = renderComponent();
 
     const component = getByTestId("PromotedLinks");
 
-    expect(component).toHaveTextContent("example content");
+    expect(component).toHaveTextContent("The first link");
   });
 });
