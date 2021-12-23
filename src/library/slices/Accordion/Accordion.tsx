157e8ef81ca9b99c5ddb3a7beca23c05f524f343
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2021-12-23 14:57:24 +0000

    Add accordion tests

--- src/library/slices/Accordion/Accordion.tsx
+++ src/library/slices/Accordion/Accordion.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 
 import { AccordionProps } from "./Accordion.types";
@@ -6,69 +5,79 @@ import * as Styles from "./Accordion.styles";
 
 import AccordionSection from "./AccordionSection";
 
-import { uniqueID } from './../../helpers/helpers';
+import { uniqueID } from "./../../helpers/helpers";
 
+const Accordion: React.FC<AccordionProps> = ({
+  sections,
+  isFilter = false,
+  withReadMore = true,
+}) => {
+  sections.map((section, i) => {
+    section.accordionSectionId = i;
+  });
 
-const Accordion: React.FC<AccordionProps> = ({ sections, isFilter = false, withReadMore = true }) => {
+  const [showControls, setShowControls] = useState(false);
+  const [openAll, setOpenAll] = useState(true);
+  const [accordionStates, setAccordionStates] = useState(sections);
 
-    sections.map((section, i) => {
-        section.accordionSectionId = i
-    });
-
-    const [showControls, setShowControls] = useState(false);
-    const [openAll, setOpenAll] = useState(true);
-    const [accordionStates, setAccordionStates] = useState(sections);
-
-    useEffect(() => {
-        const anyOpen = accordionStates.find(accordionState => accordionState.isExpanded === true);
-        (anyOpen) ? setOpenAll(false) : setOpenAll(true);
-        setShowControls(true)
-      });
-
-    const updateAccordionState = (accordionSectionId, value) => {
-
-        // when a section is toggled update the parent (this) isExpanded value
-        const newStatus = accordionStates.map(accordionState => {
-            if(accordionState.accordionSectionId === accordionSectionId) {
-                accordionState.isExpanded = value;
-            }   
-            return accordionState;
-        });
-        setAccordionStates(newStatus);
-
-        // update the text that shows open all / close all
-        (value) ? setOpenAll(false) : setOpenAll(true);
-    };
-
-    const toggleAll = () => {
-        setOpenAll(!openAll);
-        const newStatus = accordionStates.map(accordionState => {
-             accordionState.isExpanded = openAll;
-            return accordionState;
-        });
-        setAccordionStates(newStatus);
-    }
-    
-
-
-
-    const accordionId = `accordion${uniqueID()}`
-
-    return (
-        <Styles.Container data-testid="Accordion" id={accordionId}>
-            {showControls && sections.length > 1 && <Styles.AccordionControls>
-                <Styles.OpenAllButton onClick={toggleAll} type="button" aria-expanded={!openAll}>
-                    {openAll ? "Open all" : "Close all"}
-                    <Styles.VisuallyHidden> sections</Styles.VisuallyHidden>
-                </Styles.OpenAllButton>
-            </Styles.AccordionControls> }
-            {accordionStates.map((section, i) => 
-                <AccordionSection {...section} key={i} onToggle={updateAccordionState} isFilter={isFilter} withReadMore={withReadMore} />
-            )} 
-        </Styles.Container>
+  useEffect(() => {
+    const anyOpen = accordionStates.find(
+      (accordionState) => accordionState.isExpanded === true
     );
-        
+    anyOpen ? setOpenAll(false) : setOpenAll(true);
+    setShowControls(true);
+  });
+
+  const updateAccordionState = (accordionSectionId, value) => {
+    // when a section is toggled update the parent (this) isExpanded value
+    const newStatus = accordionStates.map((accordionState) => {
+      if (accordionState.accordionSectionId === accordionSectionId) {
+        accordionState.isExpanded = value;
+      }
+      return accordionState;
+    });
+    setAccordionStates(newStatus);
+
+    // update the text that shows open all / close all
+    value ? setOpenAll(false) : setOpenAll(true);
+  };
+
+  const toggleAll = () => {
+    setOpenAll(!openAll);
+    const newStatus = accordionStates.map((accordionState) => {
+      accordionState.isExpanded = openAll;
+      return accordionState;
+    });
+    setAccordionStates(newStatus);
+  };
+
+  const accordionId = `accordion${uniqueID()}`;
+
+  return (
+    <Styles.Container data-testid="Accordion" id={accordionId}>
+      {showControls && sections.length > 1 && (
+        <Styles.AccordionControls>
+          <Styles.OpenAllButton
+            onClick={toggleAll}
+            type="button"
+            aria-expanded={!openAll}
+          >
+            {openAll ? "Open all" : "Close all"}
+            <Styles.VisuallyHidden> sections</Styles.VisuallyHidden>
+          </Styles.OpenAllButton>
+        </Styles.AccordionControls>
+      )}
+      {accordionStates.map((section, i) => (
+        <AccordionSection
+          {...section}
+          key={i}
+          onToggle={updateAccordionState}
+          isFilter={isFilter}
+          withReadMore={withReadMore}
+        />
+      ))}
+    </Styles.Container>
+  );
 };
 
 export default Accordion;
-
