3a6479d4c804e94e214be77327319cdc64861064
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-01-06 14:59:44 +0000

    Update storybook to 6.4

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,47 +1,54 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@react-theming/storybook-addon",     // https://github.com/react-theming/storybook-addon
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",              // https://github.com/storybookjs/storybook/tree/master/addons/a11y
-    "@etchteam/storybook-addon-status",    // https://storybook.js.org/addons/@etchteam/storybook-addon-status/
-    "@storybook/addon-ie11"
+  core: {
+    builder: 'webpack5',
+  },
+  typescript: { reactDocgen: false },
+  features: {
+    postcss: false,
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@react-theming/storybook-addon', // https://github.com/react-theming/storybook-addon
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y', // https://github.com/storybookjs/storybook/tree/master/addons/a11y
+    '@etchteam/storybook-addon-status', // https://storybook.js.org/addons/@etchteam/storybook-addon-status/
+    '@storybook/addon-ie11',
+    '@storybook/addon-docs',
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
     });
 
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
+      loader: require.resolve('babel-loader'),
       options: {
         presets: [
-          ["react-app", { flow: false, typescript: true }],
-          ["@babel/preset-typescript"], 
-          ["@babel/preset-env", 
+          ['react-app', { flow: false, typescript: true }],
+          ['@babel/preset-typescript'],
+          [
+            '@babel/preset-env',
             {
-              "useBuiltIns": "usage",
-              "loose": true,
-              "shippedProposals": true,
-              "corejs": { "version":3 },
-              "targets": {
-                "ie": "11"
-              }
+              useBuiltIns: 'usage',
+              loose: true,
+              shippedProposals: true,
+              corejs: { version: 3 },
+              targets: {
+                ie: '11',
+              },
             },
-          ]]
-      }
+          ],
+        ],
+      },
     });
-    config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.extensions.push('.ts', '.tsx');
 
     return config;
-  }
+  },
 };
