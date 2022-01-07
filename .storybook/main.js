d901b233f630ac85b37c161fe308d020d59d9111
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-01-07 11:39:19 +0000

    npm upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,11 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  typescript: { reactDocgen: false },
   features: {
     postcss: false,
+    storyStoreV7: false,
   },
+  framework: '@storybook/react',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@react-theming/storybook-addon', // https://github.com/react-theming/storybook-addon
@@ -16,7 +17,6 @@ module.exports = {
     '@storybook/addon-a11y', // https://github.com/storybookjs/storybook/tree/master/addons/a11y
     '@etchteam/storybook-addon-status', // https://storybook.js.org/addons/@etchteam/storybook-addon-status/
     '@storybook/addon-ie11',
-    '@storybook/addon-docs',
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
