4419211e8328b2a0aa200ae17b683f7203232d65
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-01-12 13:20:28 +0000

    Center align the AlertBannerService content, keeping old max width. Add to service page examples

--- src/library/pages/SignpostPageExample/SignpostPageExample.tsx
+++ src/library/pages/SignpostPageExample/SignpostPageExample.tsx
@@ -2,102 +2,115 @@ import React from 'react';
 import * as PageStructures from '../../structure/PageStructures';
 import Heading from '../../components/Heading/Heading';
 import { hrefTo } from '@storybook/addon-links';
-import { SignpostLinkProp } from "../../structure/SignpostLinks/SignpostLinks.types"
+import { SignpostLinkProp } from '../../structure/SignpostLinks/SignpostLinks.types';
+import AlertBannerService from '../../structure/AlertBannerService/AlertBannerService';
 
 export interface SignpostPageExampleProps {
   signpostLinksArrayExample: Array<SignpostLinkProp>;
+  showServiceAlert: boolean;
 }
 
-export const SignpostPageExample: React.FC<SignpostPageExampleProps> = ({ signpostLinksArrayExample }) => (
+export const SignpostPageExample: React.FunctionComponent<SignpostPageExampleProps> = ({
+  signpostLinksArrayExample,
+  showServiceAlert = false,
+}) => (
   <>
     <PageStructures.Header />
     <PageStructures.MaxWidthContainer>
       <PageStructures.PhaseBanner />
-      <PageStructures.Breadcrumbs 
+      <PageStructures.Breadcrumbs
         breadcrumbsArray={[
           {
-              title: "Home",
-              url: "/iframe.html?id=page-examples-home-page--example-home&viewMode=story"
+            title: 'Home',
+            url: '/iframe.html?id=page-examples-home-page--example-home&viewMode=story',
           },
           {
-              title: "Bin collection, recycling and waste",
-              url: "/?path=/story/page-examples-service-landing-page-examples--bin-collection-example"
-          }
+            title: 'Bin collection, recycling and waste',
+            url: '/?path=/story/page-examples-service-landing-page-examples--bin-collection-example',
+          },
         ]}
       />
       <PageStructures.PageWithSidebarContainer>
         <PageStructures.PageMain>
+          {showServiceAlert && (
+            <AlertBannerService title="Example Service Alert" alertType="alert">
+              <p>This is an example alert</p>
+            </AlertBannerService>
+          )}
+
           <Heading level={1} text="Find your bin collection day" />
-          <p>Find your bin collection day using your post code and see your waste and recycling calendar on your local council website.</p>
-        
-          <PageStructures.SignpostLinks 
-            signpostLinksArray={signpostLinksArrayExample}
-          />
+          <p>
+            Find your bin collection day using your post code and see your waste and recycling calendar on your local
+            council website.
+          </p>
+
+          <PageStructures.SignpostLinks signpostLinksArray={signpostLinksArrayExample} />
         </PageStructures.PageMain>
         <PageStructures.PageSidebar>
           <PageStructures.SectionLinksSidebar
-            Title="Pages in this section" 
+            Title="Pages in this section"
             Sections={[
               {
-                SectionTitle: "Your bins and rubbish",
+                SectionTitle: 'Your bins and rubbish',
                 SectionLinks: [
                   {
-                      title: "Find your bin collection day",
-                      url: "/",
-                      isCurrent: true
+                    title: 'Find your bin collection day',
+                    url: '/',
+                    isCurrent: true,
                   },
                   {
-                      title: "Arrange bulky item collection",
-                      url: "/"
+                    title: 'Arrange bulky item collection',
+                    url: '/',
                   },
                   {
-                      title: "Report a missed collection",
-                      url: "/"
+                    title: 'Report a missed collection',
+                    url: '/',
                   },
                   {
-                      title: "Request a new or replacement bin",
-                      url: "/"
+                    title: 'Request a new or replacement bin',
+                    url: '/',
                   },
                   {
-                      title: "Find a household waste recycling centre",
-                      url: "/"
+                    title: 'Find a household waste recycling centre',
+                    url: '/',
                   },
                   {
-                      title: "What to recycle and where?",
-                      url: "/"
-                  }
-              ]
-            }
-          ]} />
+                    title: 'What to recycle and where?',
+                    url: '/',
+                  },
+                ],
+              },
+            ]}
+          />
         </PageStructures.PageSidebar>
       </PageStructures.PageWithSidebarContainer>
     </PageStructures.MaxWidthContainer>
-    <PageStructures.Footer 
+    <PageStructures.Footer
       footerLinksArray={[
         {
-          title: "About",
-          url: "/"
+          title: 'About',
+          url: '/',
         },
         {
-          title: "Accessibility",
-          url: "/"
+          title: 'Accessibility',
+          url: '/',
         },
         {
-          title: "Cookies",
-          url: "/"
+          title: 'Cookies',
+          url: '/',
         },
         {
-          title: "Contact us",
-          url: "/"
+          title: 'Contact us',
+          url: '/',
         },
         {
-          title: "Jobs",
-          url: "/"
+          title: 'Jobs',
+          url: '/',
         },
         {
-          title: "Newsletter",
-          url: "/"
-        }
+          title: 'Newsletter',
+          url: '/',
+        },
       ]}
     />
   </>
--- src/library/structure/AlertBannerService/AlertBannerService.styles.js
+++ src/library/structure/AlertBannerService/AlertBannerService.styles.js
@@ -1,29 +1,51 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    border: solid 4px;
-    border-radius: ${props => props.theme.theme_vars.border_radius};
-    border-color: ${props => props.alertType === "alert" ? props.theme.theme_vars.colours.negative : 
-                props.alertType === "warning" ? props.theme.theme_vars.colours.focus :
-                props.alertType === "positive" ? props.theme.theme_vars.colours.positive :
-                props.alertType === "london_bridge" ? props.theme.theme_vars.colours.black : props.theme.theme_vars.colours.negative
-    };
+  border: solid 4px;
+  border-radius: ${(props) => props.theme.theme_vars.border_radius};
+  border-color: ${(props) =>
+    props.alertType === 'alert'
+      ? props.theme.theme_vars.colours.negative
+      : props.alertType === 'warning'
+      ? props.theme.theme_vars.colours.focus
+      : props.alertType === 'positive'
+      ? props.theme.theme_vars.colours.positive
+      : props.alertType === 'london_bridge'
+      ? props.theme.theme_vars.colours.black
+      : props.theme.theme_vars.colours.negative};
 
-    padding: 15px;
-    margin-bottom: 25px;
-    p {
-        margin-bottom: 15px;
+  padding: 15px;
+  margin-bottom: 25px;
+  display: flex;
+  justify-content: center;
 
-        &:last-of-type {
-            margin-bottom: 0;
-        }
-    }
-    h2 {
-        margin-top: 0;
-    }
+  @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.s}) {
+    padding: 20px;
+    border-width: 6px;
+  }
+`;
 
-    @media screen and (min-width: ${props => props.theme.theme_vars.breakpoints.s}){
-        padding: 20px;
-        border-width: 6px;
+export const InnerContainer = styled.div`
+  @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.l}) {
+    width: 100%;
+    max-width: ${(props) => props.theme.theme_vars.breakpoints.m};
+  }
+
+  p {
+    margin-bottom: 15px;
+
+    &:last-of-type {
+      margin-bottom: 0;
     }
-`
+    @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.l}) {
+      min-width: 100%;
+    }
+  }
+
+  h2 {
+    margin-top: 0;
+    @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.l}) {
+      min-width: 100%;
+    }
+  }
+`;
