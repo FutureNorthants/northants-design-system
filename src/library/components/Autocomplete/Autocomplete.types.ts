0f75ae82c8ca5fe5f72051d86fa5d9053acfeb9b
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2022-01-14 14:51:48 +0000

    Set searchbar label value and add prop to hide it (#97)
    * Set searchbar label value and add prop to hide it
    
    * Added autocomplete label padding, tests

--- src/library/components/Autocomplete/Autocomplete.types.ts
+++ src/library/components/Autocomplete/Autocomplete.types.ts
@@ -1,81 +1,86 @@
 export interface AutocompleteProps {
-    /**
-     * ID attribute of the Downshift control (subsidiary label/input/menu list get IDs based upon this)
-     */
-    id?: string;
+  /**
+   * ID attribute of the Downshift control (subsidiary label/input/menu list get IDs based upon this)
+   */
+  id?: string;
 
-    /**
-     * Name attribute of the input control
-     */
-    name?: string;
+  /**
+   * Name attribute of the input control
+   */
+  name?: string;
 
-    /**
-     * Text content of the label for the control
-     */
-    labelText?: string;
+  /**
+   * Text content of the label for the control, if empty will use placeholder
+   */
+  labelText?: string;
 
-    /**
-     * Default value of the input control
-     */
-    value?: string;
+  /**
+   * If you want the label visually hidden (but available for screen readers)
+   */
+  hasHiddenLabel?: boolean;
 
-    /**
-     * Size of the input control
-     */
-    size?: number;
+  /**
+   * Default value of the input control
+   */
+  value?: string;
 
-    /**
-     * Placeholder within the input control
-     */
-    placeholder?: string;
+  /**
+   * Size of the input control
+   */
+  size?: number;
 
-    /**
-     * For slightly larger display in some circumstances
-     */
-    isLarge?: boolean;
+  /**
+   * Placeholder within the input control
+   */
+  placeholder?: string;
 
-    /**
-     * Set to true if there will be a button adjacent on the right (affects right-hand border radii)
-     */
-    hasAdjacentButton?: boolean;
+  /**
+   * For slightly larger display in some circumstances
+   */
+  isLarge?: boolean;
 
-    /**
-     * Controls the input control's error state
-     */
-    isErrored?: boolean;
+  /**
+   * Set to true if there will be a button adjacent on the right (affects right-hand border radii)
+   */
+  hasAdjacentButton?: boolean;
 
-    /**
-     * Text to display if input control is in error state
-     */
-    errorText?: string;
+  /**
+   * Controls the input control's error state
+   */
+  isErrored?: boolean;
 
-    /**
-     * Array of suggestion strings
-     */
-    suggestions?: string[];
+  /**
+   * Text to display if input control is in error state
+   */
+  errorText?: string;
 
-    /**
-     * Controls whether suggestions are shown immediately (if value set also)
-     */
-    showSuggestions?: boolean;
+  /**
+   * Array of suggestion strings
+   */
+  suggestions?: string[];
 
-    /**
-     * The minimum number of characters that need to be typed before matches in the suggestions list are looked for
-     */
-    minimumMatchLength?: number;
+  /**
+   * Controls whether suggestions are shown immediately (if value set also)
+   */
+  showSuggestions?: boolean;
 
-    /**
-     * The maximum number of matched suggestions shown in the dropdown list
-     */
-    maximumMatchesShown?: number;
+  /**
+   * The minimum number of characters that need to be typed before matches in the suggestions list are looked for
+   */
+  minimumMatchLength?: number;
 
-     /**
-     * Callback function for when a suggestion is selected
-     */
-    onSelect?: (selectedValue: string) => void;
+  /**
+   * The maximum number of matched suggestions shown in the dropdown list
+   */
+  maximumMatchesShown?: number;
 
-    /**
-     * Callback function for when input value changes
-     */
-    onChange?: (inputValue: string) => void;
+  /**
+   * Callback function for when a suggestion is selected
+   */
+  onSelect?: (selectedValue: string) => void;
+
+  /**
+   * Callback function for when input value changes
+   */
+  onChange?: (inputValue: string) => void;
 }
