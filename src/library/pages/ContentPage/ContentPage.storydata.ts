7ab277dc060d66cc01dac595205be06e48cac360
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-01-31 10:31:13 +0000

    Replace manual tables with map()

---
+++ src/library/pages/ContentPage/ContentPage.storydata.ts
@@ -0,0 +1,16 @@
+/** Example story data for Content Page example */
+
+export const smallTable = {
+  caption: 'Month you apply',
+  headings: ['Date', 'Rate for vehicles', 'Rate for bicycles'],
+  data: [
+    ['First 6 weeks', '£109.80 per week', '£59.10 per week'],
+    ['Next 33 weeks', '£159.80 per week', '£89.10 per week'],
+    ['Total estimated pay', '£4,282.20', '£2,182.20'],
+  ],
+};
+
+export const largeTable = {
+  caption: 'Example table with lots of data',
+  headings: ['', 'Band A', 'Band B', 'Band C', 'Band D', 'Band E', 'Band F', 'Band G', 'Band H'],
+};
