757c3f9b3c0ee864335d51655e095f97d269a3e0
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2022-01-31 10:45:12 +0000

    Added imageOverrideLogo to HomeHero, tests (#106)
    * Added imageOverrideLogo to HomeHero, tests
    
    * Fix mobile width, remove log, add alt text control

--- src/library/structure/HomeHero/HomeHero.styles.js
+++ src/library/structure/HomeHero/HomeHero.styles.js
@@ -1,19 +1,23 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Wrapper = styled.header`
-    background: ${props => props.noBackground ? "transparent" : 
-        props.theme.cardinal_name === "north" ?  (props.theme.theme_vars.colours.grey_light+"7a") : props.theme.theme_vars.colours.white};
-`
+  background: ${(props) =>
+    props.noBackground
+      ? 'transparent'
+      : props.theme.cardinal_name === 'north'
+      ? props.theme.theme_vars.colours.grey_light + '7a'
+      : props.theme.theme_vars.colours.white};
+`;
 
 export const Container = styled.div`
-    font-family: ${props => props.theme.theme_vars.fontstack};
+    font-family: ${(props) => props.theme.theme_vars.fontstack};
     overflow: hidden;
-    background: ${props => props.theme.theme_vars.colours.action}5A;
+    background: ${(props) => props.theme.theme_vars.colours.action}5A;
     padding: 30px 0;
     padding-bottom: 15px;
 
-    @media screen and (min-width: ${props => props.theme.theme_vars.breakpoints.s}){
-        background-image: url("${props => props.image}");
+    @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.s}){
+        background-image: url("${(props) => props.image}");
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
@@ -22,73 +26,82 @@ export const Container = styled.div`
             background-image: none;
         }
     }
-    @media screen and (min-width: ${props => props.theme.theme_vars.breakpoints.s}){
+    @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.s}){
         padding: 60px 0;
     }
-    @media screen and (min-width: calc(${props => props.theme.theme_vars.breakpoints.l} + 60px)){
+    @media screen and (min-width: calc(${(props) => props.theme.theme_vars.breakpoints.l} + 60px)){
         margin-right: auto;
         margin-left: auto;
         padding: 90px 0;
         max-width: 1680px;
     }
-`
+`;
 
 export const StyledMaxWidthContainer = styled.div`
-    ${props => props.theme.fontStyles}
+    ${(props) => props.theme.fontStyles}
     margin-right: 15px;
     margin-left: 15px;
 
-    @media screen and (min-width: ${props => props.theme.theme_vars.breakpoints.m}){
+    @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.m}){
         margin-right: 30px;
         margin-left: 30px;
     }
 
-    @media screen and (min-width: calc(${props => props.theme.theme_vars.breakpoints.l} + 60px)){
+    @media screen and (min-width: calc(${(props) => props.theme.theme_vars.breakpoints.l} + 60px)){
         margin-right: auto;
         margin-left: auto;
-        max-width: ${props => props.theme.theme_vars.breakpoints.l};
+        max-width: ${(props) => props.theme.theme_vars.breakpoints.l};
     }
-`
+`;
 export const HiddenH1 = styled.h1`
-    visibility: hidden;
-`
+  visibility: hidden;
+`;
 
 export const MainBox = styled.div`
-    padding: 30px;
-    background: ${props => props.theme.theme_vars.colours.white};
-    background: ${props => props.theme.theme_vars.colours.white}F2;
-    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.11);
-    -webkit-box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.11);
-    -moz-box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.11);
-    border-radius: 5px;
+  padding: 30px;
+  background: ${(props) => props.theme.theme_vars.colours.white};
+  background: ${(props) => props.theme.theme_vars.colours.white}F2;
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.11);
+  -webkit-box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.11);
+  -moz-box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.11);
+  border-radius: 5px;
 
-    @media screen and (min-width: ${props => props.theme.theme_vars.breakpoints.m}){
-        max-width: calc(50% - 60px);
-    }
-`
+  @media screen and (min-width: ${(props) => props.theme.theme_vars.breakpoints.m}) {
+    max-width: calc(50% - 60px);
+  }
+`;
 
 export const Topline = styled.p`
-    color: ${props => props.theme.theme_vars.colours.grey_dark};
-`
+  color: ${(props) => props.theme.theme_vars.colours.grey_dark};
+`;
 
 export const LogoColoured = styled.div`
+  svg {
+    margin-top: -45px;
+    max-width: 450px;
+    width: 95%;
+    height: auto;
+  }
+  &.black_logo {
     svg {
-        margin-top: -45px;
-        max-width: 450px;
-        width: 95%;
-        height: auto;
+      fill: black !important;
+      path {
+        fill: black !important;
+      }
     }
-    &.black_logo {
-        svg {
-            fill: black !important;
-            path {
-                fill: black !important;
-            }
-        }
-    }
-`
+  }
+`;
+
+export const LogoOverride = styled.div`
+  img {
+    margin-top: -45px;
+    max-width: 520px;
+    width: 100%;
+    height: auto;
+  }
+`;
 
 export const Strapline = styled.p`
-    margin-bottom: 20px;
-    margin-top: -10px;
-`
+  margin-bottom: 20px;
+  margin-top: -10px;
+`;
--- src/library/structure/HomeHero/HomeHero.tsx
+++ src/library/structure/HomeHero/HomeHero.tsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
-import { HomeHeroProps } from "./HomeHero.types";
-import * as Styles from "./HomeHero.styles";
-import GDSLogo from "../../components/logos/GDSLogo/logo";
-import NorthColoured from "../../components/logos/NorthColouredLogo/logo";
-import WestColoured from "../../components/logos/WestColouredLogo/logo";
-import LazyImage from "react-lazy-progressive-image";
-import Searchbar from "../Searchbar/Searchbar";
-import PromotedLinks from "../../components/PromotedLinks/PromotedLinks";
+import { HomeHeroProps } from './HomeHero.types';
+import * as Styles from './HomeHero.styles';
+import GDSLogo from '../../components/logos/GDSLogo/logo';
+import NorthColoured from '../../components/logos/NorthColouredLogo/logo';
+import WestColoured from '../../components/logos/WestColouredLogo/logo';
+import LazyImage from 'react-lazy-progressive-image';
+import Searchbar from '../Searchbar/Searchbar';
+import PromotedLinks from '../../components/PromotedLinks/PromotedLinks';
 
 /**
  * The Hero that should appear at the top of the home page.
@@ -18,62 +18,79 @@ const HomeHero: React.FunctionComponent<HomeHeroProps> = ({
   strapline,
   imagesArray,
   promotedLinksArray,
-  searchSuggestions = []
+  searchSuggestions = [],
+  imageOverrideLogo,
+  imageOverrideLogoAltText,
 }) => {
   const themeContext = useContext(ThemeContext);
   const [random, setRandom] = useState(999);
+  const usingMemorialTheme =
+    themeContext.theme_vars.theme_name === 'Memorial theme North' ||
+    themeContext.theme_vars.theme_name === 'Memorial theme West';
 
   useEffect(() => {
     setRandom(Math.floor(Math.random() * imagesArray.length));
-  }, []);  
+  }, []);
 
-  return(
+  return (
     <>
       <Styles.Wrapper>
         <LazyImage
-            src={random !== 999 && imagesArray[random].image1440x810 ? imagesArray[random].image1440x810 : ''}
-            placeholder={random !== 999 && imagesArray[random].image144x81 ? imagesArray[random].image144x81 : ''}
-            visibilitySensorProps={{
-                partialVisibility: true
-            }}
+          src={random !== 999 && imagesArray[random].image1440x810 ? imagesArray[random].image1440x810 : ''}
+          placeholder={random !== 999 && imagesArray[random].image144x81 ? imagesArray[random].image144x81 : ''}
+          visibilitySensorProps={{
+            partialVisibility: true,
+          }}
         >
-          {src => 
-            <Styles.Container className={random !== 999 ? "loaded" : "loading"} image={src} title={random !== 999 && imagesArray[random].imageAltText ? imagesArray[random].imageAltText : ""}>
+          {(src) => (
+            <Styles.Container
+              className={random !== 999 ? 'loaded' : 'loading'}
+              image={src}
+              title={random !== 999 && imagesArray[random].imageAltText ? imagesArray[random].imageAltText : ''}
+            >
               <Styles.StyledMaxWidthContainer>
                 <Styles.MainBox>
-                  {topline &&
-                    <Styles.Topline>{topline}</Styles.Topline>
-                  }
+                  {topline && <Styles.Topline>{topline}</Styles.Topline>}
                   <Styles.HiddenH1>{themeContext.full_name} Council</Styles.HiddenH1>
-                  <Styles.LogoColoured className={themeContext.theme_vars.theme_name === "Memorial theme North" || themeContext.theme_vars.theme_name === "Memorial theme West" ? "black_logo" : ""}>
-                    {themeContext.cardinal_name === "north" ? <NorthColoured /> : (themeContext.cardinal_name === "west" ? <WestColoured /> : <GDSLogo />)}
-                  </Styles.LogoColoured>   
-                  {strapline && 
-                    <Styles.Strapline>{strapline}</Styles.Strapline>
-                  }
-                  <Searchbar 
+                  {imageOverrideLogo && !usingMemorialTheme && (
+                    <Styles.LogoOverride>
+                      <img src={imageOverrideLogo} width="520" height="150" alt={imageOverrideLogoAltText?.trim() ? imageOverrideLogoAltText : "Logo"} />
+                    </Styles.LogoOverride>
+                  )}
+                  {(!imageOverrideLogo || usingMemorialTheme) && (
+                    <Styles.LogoColoured className={usingMemorialTheme ? 'black_logo' : ''}>
+                      {themeContext.cardinal_name === 'north' ? (
+                        <NorthColoured />
+                      ) : themeContext.cardinal_name === 'west' ? (
+                        <WestColoured />
+                      ) : (
+                        <GDSLogo />
+                      )}
+                    </Styles.LogoColoured>
+                  )}
+                  {strapline && <Styles.Strapline>{strapline}</Styles.Strapline>}
+                  <Searchbar
                     isLight
-                    isLarge 
+                    isLarge
                     placeholder="Search the site"
                     submitInfo={{
-                      postTo: "/search",
+                      postTo: '/search',
                       params: {
-                          type: "search"
-                      }
+                        type: 'search',
+                      },
                     }}
                     suggestions={searchSuggestions}
                     maximumMatchesShown={4}
                   />
                 </Styles.MainBox>
-                {promotedLinksArray.length > 0 && 
-                  <PromotedLinks promotedLinksArray={promotedLinksArray}/>
-                 }
+                {promotedLinksArray.length > 0 && <PromotedLinks promotedLinksArray={promotedLinksArray} />}
               </Styles.StyledMaxWidthContainer>
             </Styles.Container>
-          }
+          )}
         </LazyImage>
       </Styles.Wrapper>
     </>
-)};
+  );
+};
 
 export default HomeHero;
