b5a5a57a13cb0c16f78c921855858f243d43b2a6
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-02-01 16:02:06 +0000

    Add tests

--- src/library/slices/Image/Image.test.tsx
+++ src/library/slices/Image/Image.test.tsx
@@ -6,17 +6,13 @@ import { west_theme } from '../../../themes/theme_generator';
 import { ThemeProvider } from 'styled-components';
 
 describe('Image Component', () => {
-  let props: ImageProps;
-
-  beforeEach(() => {
-    props = {
-      imageSmall: 'bar',
-      imageLarge: 'bar',
-      imageAltText: 'The image alt text',
-      ratio: '4by3',
-      caption: 'The caption for the image',
-    };
-  });
+  const props: ImageProps = {
+    imageSmall: 'foo.jpg',
+    imageLarge: 'bar.jpg',
+    imageAltText: 'The image alt text',
+    ratio: '4by3',
+    caption: 'The caption for the image',
+  };
 
   const renderComponent = () =>
     render(
@@ -24,4 +20,52 @@ describe('Image Component', () => {
         <Image {...props} />
       </ThemeProvider>
     );
+
+  it('should display the image and caption', () => {
+    const { getByRole, getByTestId } = renderComponent();
+    const image = getByRole('img');
+    const imageContainer = getByTestId('ImageContainer');
+    const figure = getByRole('figure');
+
+    // Lazy image loads the placeholder image (foo.jpg)
+    expect(image).toBeVisible();
+    expect(image).toHaveAttribute('src', 'foo.jpg');
+    expect(image).toHaveAttribute('alt', 'The image alt text');
+
+    expect(imageContainer).toHaveStyle('padding-top: 75%');
+
+    expect(figure).toHaveTextContent('The caption for the image');
+  });
+});
+
+describe('Image component with wide image and no caption', () => {
+  const props: ImageProps = {
+    imageSmall: 'bar.jpg',
+    imageLarge: 'baz.jpg',
+    imageAltText: 'The image alt text',
+    ratio: '16by9',
+  };
+
+  const renderComponent = () =>
+    render(
+      <ThemeProvider theme={west_theme}>
+        <Image {...props} />
+      </ThemeProvider>
+    );
+
+  it('should display the wide image without caption', () => {
+    const { getByRole, getByTestId } = renderComponent();
+    const imageContainer = getByTestId('ImageContainer');
+    const image = getByRole('img');
+    const figure = getByRole('figure');
+
+    // Lazy image loads the placeholder image (bar.jpg)
+    expect(image).toBeVisible();
+    expect(image).toHaveAttribute('src', 'bar.jpg');
+    expect(image).toHaveAttribute('alt', 'The image alt text');
+
+    expect(imageContainer).toHaveStyle('padding-top: 56.25%');
+
+    expect(figure).not.toHaveTextContent;
+  });
 });
--- src/library/slices/Image/Image.tsx
+++ src/library/slices/Image/Image.tsx
@@ -3,6 +3,10 @@ import { ImageProps } from './Image.types';
 import * as Styles from './Image.styles';
 import LazyImage from 'react-lazy-progressive-image';
 
+/**
+ * Responsive image slice component that allows 4 by 3 or 16 by 9 images
+ * with an optional caption.
+ */
 const Image: React.FunctionComponent<ImageProps> = ({
   imageSmall,
   imageLarge,
@@ -11,7 +15,7 @@ const Image: React.FunctionComponent<ImageProps> = ({
   caption,
 }) => (
   <Styles.Container data-testid="Image">
-    <Styles.ImageContainer ratio={ratio}>
+    <Styles.ImageContainer data-testid="ImageContainer" ratio={ratio}>
       <LazyImage placeholder={imageSmall} src={imageLarge}>
         {(src) => <Styles.Image src={src} alt={imageAltText} />}
       </LazyImage>
