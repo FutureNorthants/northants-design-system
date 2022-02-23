d85b2a30fe7b0a9ec6ea0bd7c5262a0a2a2f9eb0
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2022-02-23 11:51:26 +0000

    Add iframe title, link, magic protection spell

--- src/library/slices/GoogleMap/GoogleMap.tsx
+++ src/library/slices/GoogleMap/GoogleMap.tsx
@@ -21,7 +21,7 @@ const GoogleMap: React.FunctionComponent<GoogleMapProps> = ({
 
   /* We extract the iframe source URL and check it actually goes to www.google.com/maps */
   const src_matches = iframe_html.match(/src="([^"]+)"/gi);
-  let embed_url = src_matches?.length == 1 ? src_matches[0].replace("src=\"", "") : '';
+  let embed_url = src_matches?.length == 1 ? src_matches[0].replace('src="', '') : '';
   if (embed_url) {
     const map_matches = embed_url.match(/^https:\/\/www.google.com\/maps/gi);
     embed_url = map_matches?.length == 1 ? embed_url : '';
@@ -35,31 +35,45 @@ const GoogleMap: React.FunctionComponent<GoogleMapProps> = ({
 
   return (
     <>
-      {title && <Heading level={3} text={title} />}
-      {description && (
-        <Styles.MapDescription
-          dangerouslySetInnerHTML={{
-            __html: sanitizeHtml(description),
-          }}
-        />
-      )}
-      {(!link_url || !embed_url) && (
-        <AlertBannerService alertType={'warning'} title={'Invalid map'}>
-          Embedded map contains invalid {!link_url && 'link'}
-          {!link_url && !embed_url && ' and '}
-          {!embed_url && 'iframe data'}; please report this to the content editor
-        </AlertBannerService>
-      )}
-      {cookiesAccepted && embed_url && (
-        <Styles.MapEmbed data-testid="GoogleMapContainer">
-          <Styles.MapEmbedIFrame src={embed_url} allow="fullscreen" data-testid="GoogleMapIframe" />
-        </Styles.MapEmbed>
-      )}
-      {!cookiesAccepted && link_url && (
-        <Styles.MapLink href={link_url}>
-          {link_title ? link_title : 'Google Maps link'}
-        </Styles.MapLink>
-      )}
+      <Styles.ProtectiveContainer>
+        {title && <Heading level={3} text={title} />}
+        {description && (
+          <Styles.MapDescription
+            dangerouslySetInnerHTML={{
+              __html: sanitizeHtml(description),
+            }}
+          />
+        )}
+        {(!link_url || !embed_url) && (
+          <AlertBannerService alertType={'warning'} title={'Invalid map'}>
+            Embedded map contains invalid {!link_url && 'link'}
+            {!link_url && !embed_url && ' and '}
+            {!embed_url && 'iframe data'}; please report this to the content editor
+          </AlertBannerService>
+        )}
+        {cookiesAccepted && embed_url && (
+          <Styles.MapEmbed data-testid="GoogleMapContainer">
+            <Styles.MapEmbedIFrame
+              src={embed_url}
+              data-testid="GoogleMapIframe"
+              title={title ? title : 'Embedded map'}
+              aria-hidden="true"
+            />
+          </Styles.MapEmbed>
+        )}
+        {cookiesAccepted &&
+          link_url && ( // visually hidden link for screen readers etc. as the embed isn't very accessible so hidden from them
+            <Styles.ProtectiveContainer>
+              <Styles.AccessibleMapLink href={link_url}>
+                {link_title ? link_title : 'Google Maps link'}
+              </Styles.AccessibleMapLink>
+            </Styles.ProtectiveContainer>
+          )}
+        {!cookiesAccepted &&
+          link_url && ( // visible link for all if cookies not accepted
+            <Styles.MapLink href={link_url}>{link_title ? link_title : 'Google Maps link'}</Styles.MapLink>
+          )}
+      </Styles.ProtectiveContainer>
     </>
   );
 };
