3883f8b3bb8918789aebeaf844679ed5dd56ba69
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-03-01 14:32:49 +0000

    Update WarningTextDisclaimer link text

--- src/library/slices/WarningText/WarningText.tsx
+++ src/library/slices/WarningText/WarningText.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
+import parse from 'html-react-parser';
+import { WarningTextProps } from './WarningText.types';
+import * as Styles from './WarningText.styles';
 
-import React from "react";
-import parse from "html-react-parser"
+// @TODO this needs to fit within the content border
 
-import { WarningTextProps } from "./WarningText.types";
-import * as Styles from "./WarningText.styles";
-
-// @TODO this needs to fit within the content border 
-
+/**
+ * Display text as a highlight or warning with a title
+ */
 const WarningText: React.FC<WarningTextProps> = ({ title, isWarning = false, children }) => (
-    <Styles.WarningText isWarning={isWarning}>
-        <Styles.Title isWarning={isWarning}>{title}</Styles.Title>
-        <Styles.Content>{children}</Styles.Content>
-    </Styles.WarningText>
+  <Styles.WarningText isWarning={isWarning} data-testid="WarningText">
+    <Styles.Title isWarning={isWarning}>{title}</Styles.Title>
+    <Styles.Content>{children}</Styles.Content>
+  </Styles.WarningText>
 );
 
 export default WarningText;
--- src/library/slices/WarningTextDisclaimer/WarningTextDisclaimer.tsx
+++ src/library/slices/WarningTextDisclaimer/WarningTextDisclaimer.tsx
@@ -1,15 +1,15 @@
+import React from 'react';
+import { WarningTextDisclaimerProps } from './WarningTextDisclaimer.types';
+import WarningText from '../WarningText/WarningText';
 
-import React from "react";
-
-import { WarningTextDisclaimerProps } from "./WarningTextDisclaimer.types";
-import WarningText from "../WarningText/WarningText";
-
-const WarningTextDisclaimer: React.FC<WarningTextDisclaimerProps> = () => {
-    return(
-        <WarningText title="Why information is on a different website" isWarning>
-            We are in the process of adding information to this new unitary council website. Some pages will give you a link back to a previous council website to help you find what you need. <a href={"/your-council/about-website"} title="Read the article">Read more about the changes</a>.
-        </WarningText>
-    );
-}
+const WarningTextDisclaimer: React.FunctionComponent<WarningTextDisclaimerProps> = () => {
+  return (
+    <WarningText title="Why information is on a different website" isWarning>
+      We are in the process of adding information to this new unitary council website. Some pages will give you a link
+      back to a previous council website to help you find what you need.{' '}
+      <a href={'/your-council/about-website'}>Read more about the council changes</a>.
+    </WarningText>
+  );
+};
 
 export default WarningTextDisclaimer;
