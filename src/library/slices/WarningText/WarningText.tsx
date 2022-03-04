776639bfadf8f6379fc523984cd7c9878e164131
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-03-04 14:40:30 +0000

    Remove unknown TODO comment

--- src/library/slices/WarningText/WarningText.tsx
+++ src/library/slices/WarningText/WarningText.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import parse from 'html-react-parser';
 import { WarningTextProps } from './WarningText.types';
 import * as Styles from './WarningText.styles';
-
-// @TODO this needs to fit within the content border
 
 /**
  * Display text as a highlight or warning with a title
