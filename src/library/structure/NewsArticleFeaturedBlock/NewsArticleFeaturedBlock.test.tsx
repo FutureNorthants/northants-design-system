6b4535ce23c390594f659774c4baf9ec025ea37b
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2022-03-10 13:39:35 +0000

    Make image fit tile, add tests

---
+++ src/library/structure/NewsArticleFeaturedBlock/NewsArticleFeaturedBlock.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import NewsArticleFeaturedBlock from './NewsArticleFeaturedBlock';
+import * as storydata from './NewsArticleFeaturedBlock.storydata';
+import { west_theme } from '../../../themes/theme_generator';
+import { ThemeProvider } from 'styled-components';
+
+describe('NewsArticleFeaturedBlock structure', () => {
+  it('should render correctly with everything on', () => {
+    const renderComponent = () =>
+      render(
+        <ThemeProvider theme={west_theme}>
+          <NewsArticleFeaturedBlock articles={storydata.NewsArticleData} viewAllLink={'/'} />
+        </ThemeProvider>
+      );
+
+    const { getByText, getByRole, getAllByRole } = renderComponent();
+
+    const h2 = getByRole('heading');
+    expect(h2).toBeVisible();
+    expect(h2).toHaveTextContent('News articles and press releases');
+
+    const links = getAllByRole('link');
+    expect(links).toHaveLength(storydata.NewsArticleData.length + 1);
+    links.forEach((link, index) => {
+      if (index < storydata.NewsArticleData.length) {
+        expect(link).toBeVisible();
+        expect(link).toHaveAttribute('href', storydata.NewsArticleData[index].url);
+      } else {
+        expect(link).toBeVisible();
+        expect(link).toHaveAttribute('href', '/');
+      }
+    });
+
+    const imageSpans = getAllByRole('img');
+    expect(imageSpans).toHaveLength(storydata.NewsArticleData.length);
+    imageSpans.forEach((imageSpan, index) => {
+      expect(imageSpan).toBeVisible();
+      expect(imageSpan).toHaveStyle('background-image: url(' + storydata.NewsArticleData[index].image72x41 + ')');
+      if (storydata.NewsArticleData[index].imageAltText) {
+        expect(imageSpan).toHaveAttribute('aria-label', storydata.NewsArticleData[index].imageAltText);
+      } else {
+        expect(imageSpan).toHaveAttribute('aria-label', '');
+      }
+
+      const para = getByText(storydata.NewsArticleData[index].title);
+      expect(para).toBeVisible();
+    });
+  });
+
+  it('should render correctly with everything off', () => {
+    const renderComponent = () =>
+      render(
+        <ThemeProvider theme={west_theme}>
+          <NewsArticleFeaturedBlock articles={[]} withoutTitle />
+        </ThemeProvider>
+      );
+
+    const { queryByRole, queryAllByRole } = renderComponent();
+
+    const h2 = queryByRole('heading');
+    expect(h2).toBeNull();
+
+    const links = queryAllByRole('link');
+    expect(links).toHaveLength(0);
+
+    const imageSpans = queryAllByRole('img');
+    expect(imageSpans).toHaveLength(0);
+  });
+});
--- src/library/structure/NewsArticleFeaturedBlock/NewsArticleFeaturedBlock.tsx
+++ src/library/structure/NewsArticleFeaturedBlock/NewsArticleFeaturedBlock.tsx
@@ -1,55 +1,62 @@
+import React from 'react';
+import LazyImage from 'react-lazy-progressive-image';
+import { NewsArticleFeaturedBlockProps } from './NewsArticleFeaturedBlock.types';
+import * as Styles from './NewsArticleFeaturedBlock.styles';
+import NewsArticleDate from '../NewsArticleDate/NewsArticleDate';
+import Heading from '../../components/Heading/Heading';
+import Button from '../../components/Button/Button';
 
-import React from "react";
-import LazyImage from "react-lazy-progressive-image";
-
-import { NewsArticleFeaturedBlockProps } from "./NewsArticleFeaturedBlock.types";
-import * as Styles from "./NewsArticleFeaturedBlock.styles";
-import NewsArticleDate from "../NewsArticleDate/NewsArticleDate";
-import Heading from "../../components/Heading/Heading";
-import Button from "../../components/Button/Button";
-
-const NewsArticleFeaturedBlock: React.FC<NewsArticleFeaturedBlockProps> = ({ articles, withoutTitle = false, viewAllLink ="/" }) => {
-    if(articles && articles.length > 0) {
-        return (
-            <>
-            {!withoutTitle &&
-                <Heading text="News articles and press releases" />
-            }
-            <Styles.Container>
-                {articles.map((article) => 
-                    <Styles.ArticleContainer key={article.id} href={article.url}>
-                        {article.image720x405 &&
-                            <LazyImage
-                                src={article.image720x405}
-                                placeholder={article.image72x41}
-                                visibilitySensorProps={{
-                                    partialVisibility: true
-                                }}
-                            >
-                            {src => 
-                                <Styles.ImageContainer background={src} role="img" aria-label={article.imageAltText ? article.imageAltText : ""}></Styles.ImageContainer>
-                            }
-                            </LazyImage>
-                        }
-                        <Styles.ArticleContent>
-                            <Styles.Title className="article_title">{article.title}</Styles.Title>
-                            <Styles.DateContainer><NewsArticleDate text={article.date.toString()} format="X" /></Styles.DateContainer>
-                        </Styles.ArticleContent>
-                    </Styles.ArticleContainer>
-                )}
-            </Styles.Container>
-            {viewAllLink &&
-                <Styles.ViewAllContainer>
-                    <Button text="View all news" primary={false} url={viewAllLink} size="large" />
-                </Styles.ViewAllContainer>
-            }
-            </>
-        )
-    } else {
-        return (
-            <></>
-        )
-    }
+/**
+ * Block displaying up to 9 news article tiles, with image, title and date for each
+ */
+const NewsArticleFeaturedBlock: React.FunctionComponent<NewsArticleFeaturedBlockProps> = ({
+  articles,
+  withoutTitle = false,
+  viewAllLink = '/',
+}) => {
+  if (articles && articles.length > 0) {
+    return (
+      <>
+        {!withoutTitle && <Heading text="News articles and press releases" />}
+        <Styles.Container>
+          {articles.map((article) => (
+            <Styles.ArticleContainer key={article.id} href={article.url}>
+              {article.image720x405 && (
+                <LazyImage
+                  src={article.image720x405}
+                  placeholder={article.image72x41}
+                  visibilitySensorProps={{
+                    partialVisibility: true,
+                  }}
+                >
+                  {(src) => (
+                    <Styles.ImageContainer
+                      background={src}
+                      role="img"
+                      aria-label={article.imageAltText ? article.imageAltText : ''}
+                    />
+                  )}
+                </LazyImage>
+              )}
+              <Styles.ArticleContent>
+                <Styles.Title className="article_title">{article.title}</Styles.Title>
+                <Styles.DateContainer>
+                  <NewsArticleDate text={article.date.toString()} format="X" />
+                </Styles.DateContainer>
+              </Styles.ArticleContent>
+            </Styles.ArticleContainer>
+          ))}
+        </Styles.Container>
+        {viewAllLink && (
+          <Styles.ViewAllContainer>
+            <Button text="View all news" primary={false} url={viewAllLink} size="large" />
+          </Styles.ViewAllContainer>
+        )}
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default NewsArticleFeaturedBlock;
