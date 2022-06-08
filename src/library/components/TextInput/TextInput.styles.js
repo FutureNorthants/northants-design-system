e98a1ad85d0c671d1660c233aa043bffb5b57181
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-06-08 10:04:39 +0100

    Fix search and footer links styles

--- src/library/components/TextInput/TextInput.styles.js
+++ src/library/components/TextInput/TextInput.styles.js
@@ -1,37 +1,34 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledTextInput = styled.input`
-    display: block;
+  display: block;
+  width: 100%;
+  ${(props) => props.theme.fontStyles}
+  margin-top: 0 !important;
+  margin-bottom: 0;
+  padding: ${(props) =>
+    props.isLarge ? props.theme.theme_vars.spacingSizes.small : props.theme.theme_vars.spacingSizes.extra_small};
+  border: solid
+    ${(props) => (props.isErrored ? props.theme.theme_vars.colours.negative : props.theme.theme_vars.colours.black)};
+  border-width: ${(props) =>
+    props.isErrored ? props.theme.theme_vars.border_width_error : props.theme.theme_vars.border_width};
+  border-radius: ${(props) =>
+    props.isLarge ? props.theme.theme_vars.border_radius_large : props.theme.theme_vars.border_radius};
 
-    /* 100% of container minus padding and borders */
-    width: calc(100% - ${props => props.isLarge? props.theme.theme_vars.spacingSizes.small : props.theme.theme_vars.spacingSizes.extra_small}
-                     - ${props => props.isLarge? props.theme.theme_vars.spacingSizes.small : props.theme.theme_vars.spacingSizes.extra_small}
-                     - ${props => props.theme.theme_vars.border_width}
-                     - ${props => props.theme.theme_vars.border_width});
+  &:focus {
+    outline: none;
+    box-shadow: ${(props) => props.theme.theme_vars.colours.focus} 0 0 0 3px;
+    transition: box-shadow 0.3s ease 0s;
+  }
 
-    ${props => props.theme.fontStyles}
-    margin-top: 0 !important;
-    margin-bottom: 0;
-    padding: ${props => props.isLarge ? props.theme.theme_vars.spacingSizes.small : props.theme.theme_vars.spacingSizes.extra_small};
-    border: solid ${props => props.isErrored ? props.theme.theme_vars.colours.negative : props.theme.theme_vars.colours.black};
-    border-width: ${props => props.isErrored ? props.theme.theme_vars.border_width_error : props.theme.theme_vars.border_width};
-    border-radius: ${props => props.isLarge ? props.theme.theme_vars.border_radius_large : props.theme.theme_vars.border_radius};
-
-    &:focus {
-        outline: none;
-        box-shadow: ${props => props.theme.theme_vars.colours.focus} 0 0 0 3px;
-        transition: box-shadow 0.3s ease 0s;
-    }
-
-    &::placeholder {
-        color: ${props => props.theme.theme_vars.colours.placeholder};
-        opacity: 1;
-    }
-    &:-ms-input-placeholder {
-        color: ${props => props.theme.theme_vars.colours.placeholder};
-    }
-    &::-ms-input-placeholder {
-        color: ${props => props.theme.theme_vars.colours.placeholder};
-    }
-
+  &::placeholder {
+    color: ${(props) => props.theme.theme_vars.colours.placeholder};
+    opacity: 1;
+  }
+  &:-ms-input-placeholder {
+    color: ${(props) => props.theme.theme_vars.colours.placeholder};
+  }
+  &::-ms-input-placeholder {
+    color: ${(props) => props.theme.theme_vars.colours.placeholder};
+  }
 `;
--- src/library/structure/Footer/Footer.styles.js
+++ src/library/structure/Footer/Footer.styles.js
@@ -27,6 +27,7 @@ export const FooterList = styled.ul`
 
 export const FooterListItem = styled.li`
   display: inline-block;
+  left: 0;
   margin-right: 25px;
   margin-bottom: 15px;
 `;
