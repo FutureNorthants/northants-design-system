4e7b09f1a95b6cc2d8544b9cc8aad1f27da1358c
Damien Burke <Damien.Burke@westnorthants.gov.uk>
2022-06-08 15:13:54 +0100

    Fix pagination bug, add tests (#215)

--- src/library/components/Pagination/Pagination.styles.js
+++ src/library/components/Pagination/Pagination.styles.js
@@ -1,57 +1,57 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  ${props => props.theme.fontStyles};
+  ${(props) => props.theme.fontStyles};
   display: block;
-  border-top: 1px solid ${props => props.theme.theme_vars.colours.grey};
-  padding-top: ${props => props.theme.theme_vars.spacingSizes.small};
-`
+  border-top: 1px solid ${(props) => props.theme.theme_vars.colours.grey};
+  padding-top: ${(props) => props.theme.theme_vars.spacingSizes.small};
+`;
 
 const resetButtonStyles = `
   background: transparent;
   border: none;
   cursor: pointer;
   padding: 8px 15px;
-`
+`;
 
 export const Previous = styled.button`
   ${resetButtonStyles}
-  ${props => props.theme.fontStyles}
-  ${props => props.theme.linkStyles}
+  ${(props) => props.theme.fontStyles}
+  ${(props) => props.theme.linkStyles}
 
   &:hover {
-    ${props => props.theme.linkStylesHover}
+    ${(props) => props.theme.linkStylesHover}
   }
   &:focus {
-    ${props => props.theme.linkStylesFocus}
+    ${(props) => props.theme.linkStylesFocus}
   }
   &:active {
-    ${props => props.theme.linkStylesActive}
+    ${(props) => props.theme.linkStylesActive}
   }
-`
+`;
 
 export const Next = styled.button`
   ${resetButtonStyles}
-  ${props => props.theme.fontStyles}
-  ${props => props.theme.linkStyles}
+  ${(props) => props.theme.fontStyles}
+  ${(props) => props.theme.linkStyles}
 
   &:hover {
-    ${props => props.theme.linkStylesHover}
+    ${(props) => props.theme.linkStylesHover}
   }
   &:focus {
-    ${props => props.theme.linkStylesFocus}
+    ${(props) => props.theme.linkStylesFocus}
   }
   &:active {
-    ${props => props.theme.linkStylesActive}
+    ${(props) => props.theme.linkStylesActive}
   }
-`
+`;
 
 export const NumbersContainer = styled.ul`
   margin: 0 !important;
   padding: 0;
   display: inline-block;
   list-style: none;
-`
+`;
 
 export const NumberContainer = styled.li`
   display: inline-block;
@@ -60,28 +60,29 @@ export const NumberContainer = styled.li`
   &:before {
     display: none;
   }
-`
-
+`;
 
 export const Number = styled.button`
   ${resetButtonStyles}
   padding: 8px 12px;
-  ${props => props.theme.fontStyles}
-  ${props => props.theme.linkStyles}
+  ${(props) => props.theme.fontStyles}
+  ${(props) => props.theme.linkStyles}
 
   &:hover {
-    ${props => props.theme.linkStylesHover}
+    ${(props) => props.theme.linkStylesHover}
   }
   &:focus {
-    ${props => props.theme.linkStylesFocus}
+    ${(props) => props.theme.linkStylesFocus}
   }
   &:active {
-    ${props => props.theme.linkStylesActive}
+    ${(props) => props.theme.linkStylesActive}
   }
 
-  text-decoration: ${props => props.isCurrent ? 'none' : 'underline'};
-  font-weight: ${props => props.isCurrent ? 'normal' : '700'};
-`
+  text-decoration: ${(props) => (props.isCurrent ? 'none' : 'underline')};
+  font-weight: ${(props) => (props.isCurrent ? 'normal' : '700')};
+`;
+
+export const Ellipsis = styled.span``;
 
 export const VisuallyHidden = styled.span`
   position: absolute !important;
@@ -94,4 +95,4 @@ export const VisuallyHidden = styled.span`
   clip-path: inset(50%) !important;
   border: 0 !important;
   white-space: nowrap !important;
-`
+`;
--- src/library/components/Pagination/Pagination.tsx
+++ src/library/components/Pagination/Pagination.tsx
@@ -1,87 +1,97 @@
+import React from 'react';
+import { PaginationProps } from './Pagination.types';
+import * as Styles from './Pagination.styles';
+import { handleParams } from './../../helpers/url-helpers.js';
 
-import React from "react";
-
-import { PaginationProps } from "./Pagination.types";
-import * as Styles from "./Pagination.styles";
-
-import {handleParams} from './../../helpers/url-helpers.js';
-
-
-const Pagination: React.FC<PaginationProps> = ({ currentPage, totalResults, resultsPerPage, postTo = "search" }) => {
-
-    // support for ...
-const pagination = (c,m) => {
-    var current = c,
-        last = m,
-        delta = 2,
-        left = current - delta,
-        right = current + delta + 1,
-        range = [],
-        rangeWithDots = [],
-        l;
+const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, totalResults, resultsPerPage, postTo = 'search' }) => {
+  
+  // given current page number and max page number, return list of numbers to display with ... to reduce range visible
+  const pagination = (currentPage, maxPage) => {
+    var current = currentPage;
+    var last = maxPage;
+    var delta = 2;
+    var left = current - delta;
+    var right = current + delta + 1;
+    var range = [];
+    var rangeWithDots = [];
+    var l;
 
     for (let i = 1; i <= last; i++) {
-        if (i == 1 || i == last || i >= left && i < right) {
-            range.push(i);
-        }
+      if (i == 1 || i == last || (i >= left && i < right)) {
+        range.push(i);
+      }
     }
 
     for (let i of range) {
-        if (l) {
-            if (i - l === 2) {
-                rangeWithDots.push(l + 1);
-            } else if (i - l !== 1) {
-                rangeWithDots.push('...');
-            }
+      if (l) {
+        if (i - l === 2) {
+          rangeWithDots.push(l + 1);
+        } else if (i - l !== 1) {
+          rangeWithDots.push('...');
         }
-        rangeWithDots.push(i);
-        l = i;
+      }
+      rangeWithDots.push(i);
+      l = i;
     }
 
     return rangeWithDots;
-}
-    
-if(resultsPerPage === undefined) resultsPerPage = 10;
-const numberOfNumbers = Math.ceil(totalResults / resultsPerPage);
+  };
 
+  if (resultsPerPage === undefined) {
+    resultsPerPage = 10;
+  }
+  const numberOfNumbers = Math.ceil(totalResults / resultsPerPage);
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > numberOfNumbers) {
+    currentPage = numberOfNumbers;
+  }
+  const numbers = pagination(currentPage, numberOfNumbers);
 
-const allNumbers = Array.from({ length: numberOfNumbers }, (v, i) => i + 1);
+  const buttonClick = (pageNo) => {
+    handleParams(postTo, [{ key: 'page', value: pageNo }]);
+  };
 
-const numbers = pagination(currentPage, numberOfNumbers);
-// const numbers = allNumbers;
-
-
-const buttonClick = (pageNo) => {
-    handleParams(postTo, [{key: 'page', value: pageNo}]);
-}
-
-
-if(numbers.length > 1) {
-return (
-    <Styles.Container data-testid="Pagination" role="navigation" aria-label="Pagination">
-        {currentPage !== 1 &&
-            <Styles.Previous onClick={() => buttonClick(currentPage - 1)} title="Go back a page">Previous</Styles.Previous>
-        }
+  if (numbers.length > 1) {
+    return (
+      <Styles.Container data-testid="Pagination" role="navigation" aria-label="Pagination">
+        {currentPage > 1 && (
+          <Styles.Previous onClick={() => buttonClick(currentPage - 1)} title="Go back a page">
+            Previous
+          </Styles.Previous>
+        )}
 
         <Styles.NumbersContainer>
-            {numbers.map((v,i) => 
+          {numbers.map((v, i) => (
             <Styles.NumberContainer key={i}>
-                <Styles.Number onClick={() => buttonClick(v)} isCurrent={currentPage === v}  title={currentPage === v ? "This is the current page" : v === "..." ? "See more pages" : ("Go to page " + v)}>
-                    <Styles.VisuallyHidden>Page </Styles.VisuallyHidden>
-                    {v}
+              { v === '...' && (
+                <Styles.Ellipsis>...</Styles.Ellipsis>
+              )}
+              { v !== '...' && (
+                <Styles.Number
+                  onClick={() => buttonClick(v)}
+                  isCurrent={currentPage === v}
+                  title={
+                    currentPage === v ? 'This is the current page' : 'Go to page ' + v
+                  }
+                >
+                  <Styles.VisuallyHidden>Page </Styles.VisuallyHidden>
+                  {v}
                 </Styles.Number>
+              )}                
             </Styles.NumberContainer>
-            )}      
+          ))}
         </Styles.NumbersContainer>
 
-        {currentPage < numberOfNumbers &&
-            <Styles.Next onClick={() => buttonClick(currentPage + 1)} title="Go forward a page">Next</Styles.Next>
-        }
-
-    </Styles.Container>
+        {currentPage < numberOfNumbers && (
+          <Styles.Next onClick={() => buttonClick(currentPage + 1)} title="Go forward a page">
+            Next
+          </Styles.Next>
+        )}
+      </Styles.Container>
     );
-}
-return null
-}
+  }
+  return null;
+};
 
 export default Pagination;
