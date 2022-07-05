6aa0c01deded4a6e251e80fc8facaf26af1709fb
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-07-05 13:36:33 +0100

    Remove image wrap feature

--- src/library/slices/Image/Image.styles.js
+++ src/library/slices/Image/Image.styles.js
@@ -8,10 +8,8 @@ export const Container = styled.figure`
       : props.theme.theme_vars.colours.grey_light};
   border-bottom: 1px solid ${(props) => props.theme.theme_vars.colours.grey};
   margin-bottom: ${(props) => props.theme.theme_vars.spacingSizes.medium};
-  float: ${(props) => (props.wrapText === true ? 'left' : 'none')};
-  width: ${(props) => (props.wrapText === true ? '30%' : '100%')};
+  width: 100%;
   margin-top: ${(props) => props.theme.theme_vars.spacingSizes.medium};
-  margin-right: ${(props) => (props.wrapText === true ? '1.5rem' : '0')};
 
   @media screen and (max-width: ${(props) => props.theme.theme_vars.breakpoints.s}) {
     float: none;
