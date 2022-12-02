a044e69ea13a872b5e2a56b293b102a2524a968d
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2022-12-02 14:51:36 +0000

    Fix type errors

--- src/library/slices/SearchBox/SearchBox.types.ts
+++ src/library/slices/SearchBox/SearchBox.types.ts
@@ -12,17 +12,17 @@ export interface SearchBoxProps {
   /**
    * URL of the hero image (min 400pix high)
    */
-  imageLarge?: string;
+  imageLarge?: string | null;
 
   /**
    * URL of the a smaller version of the image for lazy loading
    */
-  imageSmall?: string;
+  imageSmall?: string | null;
 
   /**
    * Optional alt text for the image - ideally the image should be decorative only and not require a long description
    */
-  imageAltText?: string;
+  imageAltText?: string | null;
 
   /**
    * The search box label
@@ -37,7 +37,7 @@ export interface SearchBoxProps {
   /**
    * Form submission method
    */
-  method: 'get' | 'post';
+  method: string;
 
   /**
    * Where to submit the form
@@ -47,7 +47,7 @@ export interface SearchBoxProps {
   /**
    * Optional placeholder text
    */
-  placeholder?: string;
+  placeholder?: string | null;
 
   /**
    * The search button text
@@ -57,12 +57,12 @@ export interface SearchBoxProps {
   /**
    * Optional call to action link
    */
-  searchBoxLink?: SearchBoxLinkProps;
+  searchBoxLink?: SearchBoxLinkProps | null;
 
   /**
    * Optional title
    */
-  title?: string;
+  title?: string | null;
 }
 
 export interface SearchBoxLinkProps {
