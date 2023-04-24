fa832455f716e64123509044797ab21d35277129
CS Rhymes <chrisrhymes@users.noreply.github.com>
2023-04-24 10:14:35 +0100

    Update birth certificate icons

--- src/library/components/icons/services/BirthCertificate/Icon.tsx
+++ src/library/components/icons/services/BirthCertificate/Icon.tsx
@@ -3,83 +3,54 @@ import { IconProps } from '../Icon.types';
 
 const ServiceIcon: React.FunctionComponent<IconProps> = ({ colourFill }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="76" viewBox="0 0 56.88 57" height="76" version="1.0">
-      <defs>
-        <clipPath id="a">
-          <path d="M0 12h44.906v40.898H0Zm0 0" />
-        </clipPath>
-        <clipPath id="b">
-          <path d="M0 8h44.906v44.898H0Zm0 0" />
-        </clipPath>
-        <clipPath id="c">
-          <path d="M0 5.746h44.906v47.152H0Zm0 0" />
-        </clipPath>
-        <clipPath id="d">
-          <path d="M0 5.746h43v47.152H0Zm0 0" />
-        </clipPath>
-        <clipPath id="e">
-          <path d="M5 5.746h32v47.152H5Zm0 0" />
-        </clipPath>
-      </defs>
-      <g clip-path="url(#a)" fill="none" stroke={colourFill} stroke-width="4" stroke-miterlimit="10">
+    <svg width="76" height="76" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76 76">
+      <g fill="none">
         <path
-          d="M19.914 41.582c0 3.145-4.715 3.145-4.715 0s4.715-3.145 4.715 0M27.46 41.582c0 3.145-4.714 3.145-4.714 0s4.715-3.145 4.715 0"
-          stroke-width="1.88604"
-        />
-      </g>
-      <g clip-path="url(#b)">
-        <path
-          fill="none"
-          d="m26.566 34.934-.05 1.933s0 2.36-2.829 2.36h-4.714c-2.828 0-2.828-2.36-2.828-2.36V34.98Zm0 0"
           stroke={colourFill}
-          stroke-width="1.88604"
+          stroke-width="2.58px"
           stroke-miterlimit="10"
+          d="M23.29,54.08c0,4.3-6.46,4.3-6.46,0s6.46-4.31,6.46,0"
         />
-      </g>
-      <g clip-path="url(#c)">
         <path
-          fill="none"
-          d="M22.746 30.266h-.473v4.714l4.293-.046-.05-.895s0-3.773-3.77-3.773Zm0 0"
           stroke={colourFill}
-          stroke-width="1.88604"
+          stroke-width="2.58px"
           stroke-miterlimit="10"
+          d="M33.62,54.08c0,4.3-6.46,4.3-6.46,0s6.46-4.31,6.46,0"
         />
-      </g>
-      <g clip-path="url(#d)">
         <path
-          fill="none"
-          d="M16.145 34.039s0-1.414-1.415-1.414h-.472"
           stroke={colourFill}
-          stroke-width="1.88604"
+          stroke-width="2.58px"
           stroke-miterlimit="10"
+          d="M32.39,45l-.06,2.65s0,3.23-3.88,3.23H22c-3.88,0-3.88-3.23-3.88-3.23V45Z"
         />
-      </g>
-      <path
-        fill="none"
-        stroke-linejoin="round"
-        d="M22.273 17.535h-8.015M28.402 22.25H14.258M28.402 25.55H14.258"
-        stroke={colourFill}
-        stroke-width="1.88604"
-        stroke-miterlimit="10"
-      />
-      <path
-        stroke-linecap="round"
-        fill="none"
-        stroke-linejoin="round"
-        d="M35.477 15.648h-4.715c-4.246 0-4.246-3.77-4.246-3.77V7.165"
-        stroke={colourFill}
-        stroke-width="1.88604"
-        stroke-miterlimit="10"
-      />
-      <g clip-path="url(#e)">
         <path
-          stroke-linecap="round"
-          fill="none"
+          stroke={colourFill}
+          stroke-width="2.58px"
+          stroke-miterlimit="10"
+          d="M27.16,38.58h-.64V45L32.39,45l-.06-1.23s0-5.16-5.17-5.16Z"
+        />
+        <path
+          stroke={colourFill}
+          stroke-width="2.58px"
+          stroke-miterlimit="10"
+          d="M18.12,43.74a1.89,1.89,0,0,0-1.93-1.93h-.65"
+        />
+        <path stroke={colourFill} stroke-width="2.58px" stroke-linejoin="round" d="M26.52,21.14h-11" />
+        <path stroke={colourFill} stroke-width="2.58px" stroke-linejoin="round" d="M34.91,27.6H15.54" />
+        <path stroke={colourFill} stroke-width="2.58px" stroke-linejoin="round" d="M34.91,32.12H15.54" />
+        <path
+          stroke={colourFill}
+          stroke-width="2.58px"
           stroke-linejoin="round"
-          d="M35.898 15.836 26.422 6.69H10.484c-3.773 0-3.773 3.774-3.773 3.774v37.719s0 3.773 3.773 3.773h21.692c3.77 0 3.77-3.773 3.77-3.773Zm0 0"
+          stroke-linecap="round"
+          d="M44.6,18.56H38.14c-5.81,0-5.81-5.17-5.81-5.17V6.94"
+        />
+        <path
           stroke={colourFill}
-          stroke-width="1.88604"
-          stroke-miterlimit="10"
+          stroke-width="2.58px"
+          stroke-linejoin="round"
+          stroke-linecap="round"
+          d="M45.18,18.82,32.2,6.29H10.37c-5.16,0-5.16,5.17-5.16,5.17V63.12s0,5.16,5.16,5.16H40.08c5.16,0,5.16-5.16,5.16-5.16l-.06-44.3Z"
         />
       </g>
     </svg>
