edfd6b05e1b45678a459970f0a9e0971a69a4bc1
CS Rhymes <chrisrhymes@users.noreply.github.com>
2023-09-06 10:14:40 +0100

    Match the other social icons

--- src/library/components/icons/TwitterIcon/Icon.tsx
+++ src/library/components/icons/TwitterIcon/Icon.tsx
@@ -3,13 +3,11 @@ import { IconProps } from '../services/Icon.types';
 
 const ServiceIcon: React.FunctionComponent<IconProps> = ({ colourFill }) => {
   return (
-    <svg width="36" height="36" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" fill="none">
-      <g>
-        <path
-          d="m.49,4l11.42,15.27L.42,31.68h2.59l10.06-10.87,8.13,10.87h8.8l-12.06-16.13,10.7-11.56h-2.59l-9.27,10.01-7.49-10.01H.49Zm3.8,1.92h4.04l17.86,23.87h-4.04L4.3,5.92Z"
-          fill={colourFill}
-        />
-      </g>
+    <svg width="36" height="36" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+      <path
+        d="m0,5.14v25.72c0,2.84,2.31,5.14,5.14,5.14h25.72c2.84,0,5.14-2.31,5.14-5.14V5.14c0-2.83-2.31-5.14-5.14-5.14H5.14C2.31,0,0,2.31,0,5.14Zm29.01,1.61l-8.34,9.53,9.81,12.97h-7.68l-6.01-7.86-6.88,7.86h-3.81l8.92-10.19L5.59,6.75h7.87l5.45,7.19,6.29-7.19s3.81,0,3.81,0Zm-3.03,20.17L12.31,8.84h-2.27l13.83,18.08h2.13Z"
+        fill={colourFill}
+      />
     </svg>
   );
 };
