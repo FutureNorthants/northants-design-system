c494505c42d3fbbd72c44771f88df1f3a97d6fea
CS Rhymes <chrisrhymes@users.noreply.github.com>
2024-09-04 16:39:19 +0100

    Add year and month to DropDownFilter

--- src/library/components/DropDownFilter/DropDownFilter.tsx
+++ src/library/components/DropDownFilter/DropDownFilter.tsx
@@ -1,26 +1,38 @@
+import React, { useState } from 'react';
+import { DropDownFilterProps } from './DropDownFilter.types';
+import * as Styles from './DropDownFilter.styles';
+import { NewsArticleFilterFields } from './../../structure/NewsArticleFilterAccordion/NewsArticleFilterAccordionText';
+import { handleParams } from './../../helpers/url-helpers.js';
+import DropDownSelect from '../DropDownSelect/DropDownSelect';
 
-import React, {useState} from "react";
+const DropDownFilter: React.FunctionComponent<DropDownFilterProps> = ({
+  id,
+  label,
+  options,
+  selected,
+  hideLabel = false,
+  filterKey = 'services',
+  hasMargin = false,
+}) => {
+  const [value, setValue] = useState(selected);
 
-import { DropDownFilterProps } from "./DropDownFilter.types";
-import * as Styles from "./DropDownFilter.styles";
+  const optionPicked = (e) => {
+    setValue(e.target.value);
+    handleParams('news', [{ key: NewsArticleFilterFields[filterKey].queryParamKey, value: e.target.value }], ['page']);
+  };
 
-import { NewsArticleFilterFields } from "./../../structure/NewsArticleFilterAccordion/NewsArticleFilterAccordionText"
-
-import {handleParams} from './../../helpers/url-helpers.js';
-import DropDownSelect from "../DropDownSelect/DropDownSelect";
-
-const DropDownFilter: React.FC<DropDownFilterProps> = ({ id, label, options, selected, hideLabel=false}) => {
-
-    const [value, setValue] = useState(selected);
-    
-    const optionPicked = (e) => {
-        setValue(e.target.value)
-        handleParams('news', [{key: NewsArticleFilterFields.services.queryParamKey, value: e.target.value}],["page"]);
-    }
-
-    return (
-        <DropDownSelect id={id} label={label} options={options} onChange={optionPicked} selected={value} hideLabel={hideLabel} />
-)};
+  return (
+    <Styles.Container $hasMargin={hasMargin}>
+      <DropDownSelect
+        id={id}
+        label={label}
+        options={options}
+        onChange={optionPicked}
+        selected={value}
+        hideLabel={hideLabel}
+      />
+    </Styles.Container>
+  );
+};
 
 export default DropDownFilter;
-
--- src/library/structure/NewsArticleFilterAccordion/NewsArticleFilterAccordionText.js
+++ src/library/structure/NewsArticleFilterAccordion/NewsArticleFilterAccordionText.js
@@ -1,20 +1,28 @@
 export const NewsArticleFilterFields = {
-    search: {
-        title: "Search articles",
-        queryParamKey: "searchTerm"
-    },
-    services: {
-        title: "Services",
-        queryParamKey: "services"
-    },
-    articleType: {
-        title: "Type of Article",
-        queryParamKey: "articleType"
-    },
-    sortBy: {
-        title: "Sort by",
-        queryParamKey: "sortBy"
-    },
-    // what is included in the filter removal
-    removeFiltersList: ['searchTerm', 'services', 'articleType']
-}
+  search: {
+    title: 'Search articles',
+    queryParamKey: 'searchTerm',
+  },
+  services: {
+    title: 'Services',
+    queryParamKey: 'services',
+  },
+  articleType: {
+    title: 'Type of Article',
+    queryParamKey: 'articleType',
+  },
+  year: {
+    title: 'Year',
+    queryParamKey: 'year',
+  },
+  month: {
+    title: 'Month',
+    queryParamKey: 'month',
+  },
+  sortBy: {
+    title: 'Sort by',
+    queryParamKey: 'sortBy',
+  },
+  // what is included in the filter removal
+  removeFiltersList: ['searchTerm', 'services', 'articleType', 'month', 'year'],
+};
