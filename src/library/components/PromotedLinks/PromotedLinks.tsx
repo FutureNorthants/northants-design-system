810a3f3c223a1cdf7566e52d643e2d1acac68cb2
CS Rhymes <chrisrhymes@users.noreply.github.com>
2024-10-18 15:48:09 +0100

    Set default value for hasTabs to false

--- src/library/components/PromotedLinks/PromotedLinks.tsx
+++ src/library/components/PromotedLinks/PromotedLinks.tsx
@@ -4,7 +4,11 @@ import * as Styles from './PromotedLinks.styles';
 import Row from '../../components/Row/Row';
 import Column from '../../components/Column/Column';
 
-const PromotedLinks: React.FunctionComponent<PromotedLinksProps> = ({ promotedLinksArray, oneCol, hasTabs }) => {
+const PromotedLinks: React.FunctionComponent<PromotedLinksProps> = ({
+  promotedLinksArray,
+  oneCol,
+  hasTabs = false,
+}) => {
   if (promotedLinksArray.length > 0) {
     return (
       <Styles.PromotedLinks data-testid="PromotedLinks" $oneCol={oneCol} $hasTabs={hasTabs}>
--- src/library/components/logos/NorthBlackLogo/logo.types.ts
+++ src/library/components/logos/NorthBlackLogo/logo.types.ts
@@ -1,4 +1,1 @@
-
-export interface LogoProps {
-
-}
+export interface LogoProps {}
