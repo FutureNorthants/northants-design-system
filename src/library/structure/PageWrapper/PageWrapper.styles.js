5805ff8ba5997f4a798737399576ec2b34ee59e1
CS Rhymes <chrisrhymes@users.noreply.github.com>
2024-11-20 09:02:47 +0000

    Fix bug with named prop in page wrapper

--- src/library/structure/PageWrapper/PageWrapper.styles.js
+++ src/library/structure/PageWrapper/PageWrapper.styles.js
@@ -1,25 +1,19 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components';
 
-
-const backgroundDecider = props => {
-
-    const colourString = props.theme.theme_vars.colours[props.colour]
-    if (props.colour){ 
-      return css`
+const backgroundDecider = (props) => {
+  const colourString = props.theme.theme_vars.colours[props.$colour];
+  if (props.colour) {
+    return css`
       background-color: ${colourString};
-      `
-    }
-    else {
-        return css`
-        background-color: transparent;
-        `
-    }
-  } 
-
+    `;
+  } else {
+    return css`
+      background-color: transparent;
+    `;
+  }
+};
 
 export const Container = styled.div`
- ${backgroundDecider};
- padding-top: 1.5em;
-`
-
-
+  ${backgroundDecider};
+  padding-top: 1.5em;
+`;
--- src/library/structure/PageWrapper/PageWrapper.tsx
+++ src/library/structure/PageWrapper/PageWrapper.tsx
@@ -1,12 +1,11 @@
+import React from 'react';
+import { PageWrapperProps } from './PageWrapper.types';
+import * as Styles from './PageWrapper.styles';
 
-import React from "react";
-
-import { PageWrapperProps } from "./PageWrapper.types";
-import * as Styles from "./PageWrapper.styles";
-
-const PageWrapper: React.FC<PageWrapperProps> = ({ colour, children }) => (
-    <Styles.Container data-testid="PageWrapper" colour={colour}>{children}</Styles.Container>
+const PageWrapper: React.FunctionComponent<PageWrapperProps> = ({ colour, children }) => (
+  <Styles.Container data-testid="PageWrapper" $colour={colour}>
+    {children}
+  </Styles.Container>
 );
 
 export default PageWrapper;
-
