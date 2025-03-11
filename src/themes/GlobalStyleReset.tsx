0ca9aadf7051add7e7ceeb7c2f52644b17f1bb1f
CS Rhymes <chrisrhymes@users.noreply.github.com>
2025-03-11 08:34:58 +0000

    Adjust line height for list items with links

--- src/themes/GlobalStyleReset.tsx
+++ src/themes/GlobalStyleReset.tsx
@@ -133,9 +133,14 @@ export const GlobalStyleReset: any = createGlobalStyle<any>`
     left: 1.25rem;
     padding-right: 1.25rem;
   }
+  main li {
+    line-height: 44px;
+  }
   main li > a {
-    display: inline-block;
+    display: inline-flex;
     min-height: 44px;
+    justify-content: center;
+    align-items: center;
   }
   blockquote,
   q {
