f68d3396ba07286c1bb166b6103867ccaa0f3d65
CS Rhymes <chrisrhymes@users.noreply.github.com>
2025-04-03 15:04:11 +0100

    Add Cludo Search component

---
+++ src/library/structure/CludoSearch/CludoSearch.styles.js
@@ -0,0 +1,193 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: block;
+
+  a {
+    text-decoration: none !important;
+    font-weight: normal;
+
+    b {
+      font-weight: bold;
+    }
+  }
+
+  .wnc-cludo-input {
+    padding-left: 0;
+    padding-right: 0;
+    margin-bottom: ${(props) => props.theme.theme_vars.spacingSizes.medium};
+    display: flex;
+    flex-direction: row;
+  }
+
+  .wnc-cludo-input input {
+    padding: 10px;
+    min-height: 44px;
+    border: 2px solid ${(props) => props.theme.theme_vars.colours.black};
+    border-radius: 6px 0 0 6px;
+    flex-grow: 1;
+    font-size: ${(props) => props.theme.theme_vars.fontSizes.small};
+  }
+
+  .wnc-cludo-input input:focus,
+  .wnc-cludo-input input:active {
+    border: 2px solid ${(props) => props.theme.theme_vars.colours.black};
+    border-radius: 6px 0 0 6px;
+    box-shadow: 0 0 0 1px #e2ca76, 0 0 0 3px #000;
+  }
+
+  .wnc-cludo-input button {
+    height: 46px;
+    width: 46px;
+    margin-left: 0;
+    border: 2px solid ${(props) => props.theme.theme_vars.colours.black};
+    border-radius: 0 6px 6px 0;
+    background-color: ${(props) => props.theme.theme_vars.colours.action};
+
+    &:hover {
+      background-color: ${(props) => props.theme.theme_vars.action_dark};
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
+
+  .wnc-cludo-facet-list {
+    margin-bottom: ${(props) => props.theme.theme_vars.spacingSizes.medium};
+
+    h3 {
+      ${(props) => props.theme.theme_vars.h3};
+    }
+  }
+
+  .wnc-cludo-facet-list ul {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 0;
+
+    li {
+      padding-right: 0;
+      left: 0;
+      line-height: 1.5;
+
+      a {
+        border: 2px solid ${(props) => props.theme.theme_vars.colours.action};
+        border-radius: ${(props) => props.theme.theme_vars.border_radius};
+        padding: ${(props) =>
+          `${props.theme.theme_vars.spacingSizes.extra_small} ${props.theme.theme_vars.spacingSizes.small}`};
+        color: ${(props) => props.theme.theme_vars.colours.action};
+        font-size: ${(props) => props.theme.theme_vars.fontSizes.small};
+
+        &.cludo-active {
+          background-color: ${(props) => props.theme.theme_vars.colours.action};
+          color: ${(props) => props.theme.theme_vars.colours.white};
+        }
+      }
+    }
+  }
+
+  .cludo_results-list li {
+    width: 100%;
+    left: 0 !important;
+    line-height: 1.5;
+    padding-right: 0;
+    margin-bottom: ${(props) => props.theme.theme_vars.spacingSizes.x_large};
+  }
+
+  .wnc-cludo-result {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: flex-start;
+  }
+
+  .cludo-banner {
+    background-color: ${(props) => props.theme.theme_vars.colours.action_light};
+    color: ${(props) => props.theme.theme_vars.colours.black};
+    width: 100%;
+    padding: ${(props) => props.theme.theme_vars.spacingSizes.small};
+  }
+
+  .wnc-cludo-result {
+    display: block;
+    &:hover {
+      .wnc-cludo-title {
+        ${(props) => props.theme.linkStylesHover};
+      }
+    }
+
+    &:focus {
+      ${(props) => props.theme.linkStylesFocus};
+    }
+
+    &:active {
+      ${(props) => props.theme.linkStylesActive};
+    }
+  }
+
+  .wnc-cludo-title {
+    color: ${(props) => props.theme.theme_vars.colours.action};
+    font-weight: 700;
+    text-decoration: underline;
+    margin-bottom: 0;
+    font-size: 1.2em;
+  }
+
+  .wnc-cludo-description {
+    font-size: ${(props) => props.theme.theme_vars.fontSizes.small};
+    color: ${(props) => props.theme.theme_vars.colours.black};
+  }
+
+  .wnc-cludo-badge {
+    background-color: ${(props) => props.theme.theme_vars.colours.action};
+    color: ${(props) => props.theme.theme_vars.colours.white};
+    padding: ${(props) => props.theme.theme_vars.spacingSizes.extra_small};
+    border-radius: ${(props) => props.theme.theme_vars.border_radius};
+    font-size: ${(props) => props.theme.theme_vars.fontSizes.small};
+    margin-bottom: ${(props) => props.theme.theme_vars.spacingSizes.small};
+    display: inline-block;
+  }
+
+  .wnc-cludo-result-link {
+    font-size: ${(props) => props.theme.theme_vars.fontSizes.extra_small};
+  }
+
+  .wnc-cludo-pagination {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-left: 0;
+
+    li {
+      left: 0%;
+      padding-right: ${(props) => props.theme.theme_vars.spacingSizes.small};
+
+      button {
+        background-color: ${(props) => props.theme.theme_vars.colours.action};
+        color: ${(props) => props.theme.theme_vars.colours.white};
+        outline: none;
+        border: none;
+        padding: 15px 15px;
+        border-radius: ${(props) => props.theme.theme_vars.border_radius};
+        cursor: pointer;
+
+        &:hover {
+          background-color: ${(props) => props.theme.theme_vars.colours.action_dark};
+        }
+
+        &:focus,
+        &:active {
+          background-color: ${(props) => props.theme.theme_vars.colours.focus};
+          box-shadow: 0px -3px 0px 0px ${(props) => props.theme.theme_vars.colours.black} inset;
+          color: ${(props) => props.theme.theme_vars.colours.black};
+        }
+      }
+    }
+  }
+`;
---
+++ src/library/structure/CludoSearch/CludoSearch.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { CludoSearchProps } from './CludoSearch.types';
+import * as Styles from './CludoSearch.styles';
+import {
+  CludoSearchOptions,
+  CludoWrapper,
+  CustomResult,
+  DidYouMean,
+  FacetGroup,
+  Pagination,
+  ResultBadge,
+  ResultCount,
+  ResultDescription,
+  ResultImage,
+  ResultsList,
+  ResultsPerPage,
+  ResultTitle,
+  ResultUrl,
+  SearchInput,
+  StandardFacet,
+} from '@cludosearch/cludo-search-components';
+
+const CludoSearch: React.FunctionComponent<CludoSearchProps> = ({ searchTerm, customerId, engineId }) => {
+  const cludoSearchConfig: CludoSearchOptions = {
+    customerId: customerId,
+    engineId: engineId,
+    searchUrl: '/',
+    language: 'en',
+    facets: {
+      keys: ['Category'],
+    },
+    behavior: {
+      enableRelatedSearches: true,
+    },
+  };
+  return (
+    <Styles.Container data-testid="CludoSearch">
+      <CludoWrapper config={cludoSearchConfig}>
+        <SearchInput className="wnc-cludo-input" formId="search" ariaLabel="Search the website" />
+        <ResultCount />
+        <DidYouMean />
+
+        <FacetGroup>
+          <StandardFacet
+            field="Category"
+            hideClear
+            hideCount
+            hideSearchBar
+            hideShowMore
+            className="wnc-cludo-facet-list"
+          />
+        </FacetGroup>
+
+        <ResultsList
+          template={(currResult) => (
+            <CustomResult result={currResult} wrapWithLink={true} className="wnc-cludo-result">
+              <ResultTitle className="wnc-cludo-title" />
+              <ResultImage />
+              <ResultDescription maxWordCount={20} className="wnc-cludo-description" />
+              <ResultBadge className="wnc-cludo-badge" />
+              <ResultUrl className="wnc-cludo-result-link" />
+            </CustomResult>
+          )}
+        />
+        <Pagination className="wnc-cludo-pagination" />
+      </CludoWrapper>
+    </Styles.Container>
+  );
+};
+
+export default CludoSearch;
