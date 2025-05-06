4c346c8b8cfa580c47297fe5c3bb1d5ad308c311
CS Rhymes <chrisrhymes@users.noreply.github.com>
2025-05-06 12:01:03 +0100

    Add additional tests for quick exit and prettier formatting for icon

--- src/library/components/QuickExit/QuickExit.test.tsx
+++ src/library/components/QuickExit/QuickExit.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { west_theme } from '../../../themes/theme_generator';
 import QuickExit from './QuickExit';
@@ -7,9 +7,17 @@ import { QuickExitProps } from './QuickExit.types';
 
 describe('Test Component', () => {
   let props: QuickExitProps;
+  // Mock the window.location.replace method
+  const mockReplace = jest.fn();
 
   beforeEach(() => {
     props = {};
+
+    Object.defineProperty(window, 'location', {
+      value: {
+        replace: mockReplace,
+      },
+    });
   });
 
   const renderComponent = () =>
@@ -25,5 +33,38 @@ describe('Test Component', () => {
     const component = getByTestId('QuickExit');
 
     expect(component).toHaveTextContent('Quick exit');
+  });
+
+  it('should redirect to default url', async () => {
+    const { getByRole } = renderComponent();
+
+    const exitButton = getByRole('button');
+
+    fireEvent.click(exitButton);
+
+    expect(mockReplace).toHaveBeenCalledWith('https://www.bbc.co.uk/weather');
+  });
+
+  it('should render custom label text correctly', () => {
+    props.label = 'Click to exit the page';
+
+    const { getByRole } = renderComponent();
+
+    const exitButton = getByRole('button');
+
+    expect(exitButton).not.toHaveTextContent('Quick exit');
+    expect(exitButton).toHaveTextContent('Click to exit the page');
+  });
+
+  it('should redirect to custom url', async () => {
+    props.url = 'https://www.westnorthants.gov.uk';
+
+    const { getByRole } = renderComponent();
+
+    const exitButton = getByRole('button');
+
+    fireEvent.click(exitButton);
+
+    expect(mockReplace).toHaveBeenCalledWith('https://www.westnorthants.gov.uk');
   });
 });
