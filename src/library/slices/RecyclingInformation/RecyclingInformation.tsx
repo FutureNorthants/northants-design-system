85e9aac81e85951a7e71426ab7fc8aaa61e3dfb6
mstanford-wnc <matthew.stanford@westnorthants.gov.uk>
2025-06-02 14:56:37 +0100

    Tweaked and standardised colours

--- src/library/slices/RecyclingInformation/RecyclingInformation.tsx
+++ src/library/slices/RecyclingInformation/RecyclingInformation.tsx
@@ -19,14 +19,18 @@ const RecyclingInformation: React.FunctionComponent<RecyclingInformationProps> =
          --theme-c9-heading-color: ${themeContext.theme_vars.colours.black};
          --theme-c9-link-color: ${themeContext.theme_vars.colours.action};
          --theme-c9-input-text-color: ${themeContext.theme_vars.colours.black};
-         --theme-c9-input-border-color: ${themeContext.theme_vars.colours.black};
-         --theme-c9-card-color: ${themeContext.theme_vars.colours.white};
+         --theme-c9-input-border-color: ${themeContext.theme_vars.colours.grey};       
          --theme-c9-card-border-radius: ${themeContext.theme_vars.border_radius};
-         --theme-c9-disposal-options-card-color: ${themeContext.theme_vars.colours.grey_light};
+         --theme-c9-disposal-options-card-color: ${themeContext.theme_vars.colours.action_dark};
          --theme-c9-disposal-options-card-border-radius: ${themeContext.theme_vars.border_radius};
          --theme-c9-body-font-family: ${themeContext.theme_vars.fontstack};
          --theme-c9-heading-font-family: ${themeContext.theme_vars.fontstack};
-         overflow-y: visible !important"/>`;
+         --theme-c9-card-border: 1px solid ${themeContext.theme_vars.colours.grey_light};        
+         --theme-c9-card-box-shadow: 2px 2px 5px ${themeContext.theme_vars.colours.grey};
+         --theme-c9-item-highlight-color: ${themeContext.theme_vars.colours.secondary};
+         --theme-c9-background-color: ${themeContext.theme_vars.colours.white};
+         --theme-c9-card-color: ${themeContext.theme_vars.colours.grey_light};
+"/>`;
 
   return (
     <Styles.Container data-testid="RecyclingInformation">
