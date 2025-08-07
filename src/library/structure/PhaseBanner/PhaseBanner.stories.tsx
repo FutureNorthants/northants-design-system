3c08f4c9e6a6447cb1a925c6881eefac7a0b59a0
mstanford-wnc <matthew.stanford@westnorthants.gov.uk>
2025-08-07 11:36:01 +0100

    Converted PhaseBanner stories to CSF3 format

--- src/library/structure/PhaseBanner/PhaseBanner.stories.tsx
+++ src/library/structure/PhaseBanner/PhaseBanner.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import PhaseBanner from './PhaseBanner';
-import { PhaseBannerProps } from './PhaseBanner.types';
 
 const meta: Meta<typeof PhaseBanner> = {
   title: 'library/Structure/Phase Banner',
@@ -14,53 +13,43 @@ const meta: Meta<typeof PhaseBanner> = {
 };
 
 export default meta;
-type Story = StoryObj<typeof PhaseBanner>;
+type Story = StoryObj<typeof meta>;
 
-export const PhaseBannerExampleWithDefaults = Template.bind({});
+export const PhaseBannerExampleWithDefaults: Story = {};
 
-export const PhaseBannerHomeExampleWithDefaults = Template.bind({});
-PhaseBannerHomeExampleWithDefaults.args = {
-  isHome: true,
+export const PhaseBannerHomeExampleWithDefaults: Story = {
+  args: { isHome: true },
 };
 
-export const PhaseBannerCustomTitleAndTextExample = Template.bind({});
-PhaseBannerCustomTitleAndTextExample.args = {
-  ...PhaseBannerExampleWithDefaults,
-  phaseTitle: 'Our trendy new website',
-  children: <p>We're where it's at</p>,
+export const PhaseBannerCustomTitleAndTextExample: Story = {
+  args: {
+    phaseTitle: 'Our trendy new website',
+    children: <p>We're where it's at</p>,
+  },
 };
 
-export const PhaseBannerCustomTitleAndTextHomeExample = Template.bind({});
-PhaseBannerCustomTitleAndTextHomeExample.args = {
-  ...PhaseBannerCustomTitleAndTextExample,
-  isHome: true,
+export const PhaseBannerCustomTitleAndTextHomeExample: Story = {
+  args: { ...PhaseBannerCustomTitleAndTextExample.args, isHome: true },
 };
 
-export const PhaseBannerCustomisedWithHyperlinkExample2 = Template.bind({});
-PhaseBannerCustomisedWithHyperlinkExample2.args = {
-  isHome: true,
-  phaseTitle: 'Beta',
-  children: (
-    <p>
-      You are using our new search experience. We are still refining how this works so you may notice some changes. To
-      help us improve it,&nbsp;
-      <a href="https://forms.westnorthants.gov.uk/xfp/form/556?FormName=West%20Northamptonshire%20Website%20Search">
-        we would love your feedback
-      </a>
-    </p>
-  ),
+export const PhaseBannerCustomisedWithHyperlinkExample2: Story = {
+  args: {
+    phaseTitle: 'Beta',
+    children: (
+      <p>
+        You are using our new search experience. We are still refining how this works so you may notice some changes. To
+        help us improve it,&nbsp;
+        <a href="https://forms.westnorthants.gov.uk/xfp/form/556?FormName=West%20Northamptonshire%20Website%20Search">
+          we would love your feedback
+        </a>
+      </p>
+    ),
+  },
 };
 
-export const PhaseBannerCustomisedWithHyperlinkExample3 = Template.bind({});
-PhaseBannerCustomisedWithHyperlinkExample3.args = {
-  phaseTitle: 'Beta',
-  children: (
-    <p>
-      You are using our new search experience. We are still refining how this works so you may notice some changes. To
-      help us improve it,&nbsp;
-      <a href="https://forms.westnorthants.gov.uk/xfp/form/556?FormName=West%20Northamptonshire%20Website%20Search">
-        we would love your feedback
-      </a>
-    </p>
-  ),
+export const PhaseBannerCustomisedWithHyperlinkExample3: Story = {
+  args: {
+    ...PhaseBannerCustomisedWithHyperlinkExample2.args,
+    isHome: true,
+  },
 };
