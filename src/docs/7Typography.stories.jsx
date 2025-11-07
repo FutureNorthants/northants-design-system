aa7b4769c3066dbd5d869fa2e55827d3acb47357
mstanford-wnc <matthew.stanford@westnorthants.gov.uk>
2025-11-07 16:39:04 +0000

    Removed references to Quicksand and Rubik fonts in Google API path

--- src/docs/7Typography.stories.jsx
+++ src/docs/7Typography.stories.jsx
@@ -18,10 +18,7 @@ export const Typography = () => {
       <PageMain>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,100..900;1,100..900&family=Quicksand:wght@300..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,100..900;1,100..900" rel="stylesheet" />
         <Row>
           {themeContext.theme_vars.fontstack.split(',').map((font) => (
             <>
